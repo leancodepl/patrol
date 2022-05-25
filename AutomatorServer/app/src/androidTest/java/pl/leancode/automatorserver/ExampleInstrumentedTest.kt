47ae0d1831a5e43e7d846965f868690925d1b3ba
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2022-05-25 13:51:21 +0200

    Initial commit

---
+++ AutomatorServer/app/src/androidTest/java/pl/leancode/automatorserver/ExampleInstrumentedTest.kt
@@ -0,0 +1,34 @@
+package pl.leancode.automatorserver
+
+import android.os.SystemClock
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class AutomatorServer {
+    @Test
+    fun startServer() {
+        Logger.addLogAdapter(AndroidLogAdapter())
+
+        if (serverInstrumentation == null) {
+            serverInstrumentation = ServerInstrumentation.instance
+            Logger.i("Starting server")
+            try {
+                serverInstrumentation!!.startServer()
+                while (!serverInstrumentation!!.isStopped) {
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Logger.w("Exception thrown: ", e)
+            }
+        }
+    }
+
+    companion object {
+        private var serverInstrumentation: ServerInstrumentation? = null
+    }
+}
