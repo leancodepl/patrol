3c0f54e3411ccfbb99591e3591a17da03c44508b
Bartek Pacia <barpac02@gmail.com>
2022-06-01 01:35:31 +0200

    debugging

--- packages/maestro/example/test_driver/integration_test.dart
+++ packages/maestro/example/test_driver/integration_test.dart
@@ -3,11 +3,10 @@ import 'package:maestro/maestro.dart';
 
 Future<void> main() async {
   print('Waiting for automator server');
-  Automator.init();
+  Automator.init(verbose: true);
   while (!await Automator.instance.isRunning()) {}
   print('Automator server is running, starting test drive');
   try {
-    print('here 0');
     await integrationDriver();
   } finally {
     print('Stopping automator server');
