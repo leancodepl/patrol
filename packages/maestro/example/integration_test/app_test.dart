78f769b3f0ce5756f1bf34dc52dee24f5c3dd8fc
Bartek Pacia <barpac02@gmail.com>
2022-05-31 22:09:39 +0200

    move example to packages/maestro/example

--- packages/maestro/example/integration_test/app_test.dart
+++ packages/maestro/example/integration_test/app_test.dart
@@ -2,7 +2,7 @@ import 'package:example/main.dart' as app;
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:maestro/automator.dart';
+import 'package:maestro/maestro.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
