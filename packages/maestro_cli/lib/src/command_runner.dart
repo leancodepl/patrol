fb3171900e7f2532592a1c2d425cf392aed403ce
Bartek Pacia <barpac02@gmail.com>
2022-06-02 00:01:39 +0200

    don't use package:logger

--- packages/maestro_cli/lib/src/command_runner.dart
+++ packages/maestro_cli/lib/src/command_runner.dart
@@ -1,16 +1,9 @@
 import 'package:args/command_runner.dart';
-import 'package:logging/logging.dart';
 import 'package:maestro_cli/src/commands/bootstrap_command.dart';
 import 'package:maestro_cli/src/commands/drive_command.dart';
+import 'package:maestro_cli/src/logging.dart';
 
 Future<int> maestroCommandRunner(List<String> args) async {
-  final logger = Logger('maestr');
-
-  Logger.root.onRecord.listen((event) {
-    // ignore: avoid_print
-    print(event.message);
-  });
-
   final runner = CommandRunner<int>(
     'maestro',
     'Tool for running Flutter-native UI tests with superpowers',
@@ -22,7 +15,7 @@ Future<int> maestroCommandRunner(List<String> args) async {
   try {
     exitCode = await runner.run(args) ?? 0;
   } on UsageException catch (err) {
-    logger.warning('Error: ${err.message}');
+    error('Error: ${err.message}');
 
     exitCode = 1;
   }
