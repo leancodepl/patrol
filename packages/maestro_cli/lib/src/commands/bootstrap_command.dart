ff254db5a475141cbdb5d1c761d9935e7d366f7e
Bartek Pacia <barpac02@gmail.com>
2022-06-02 16:31:31 +0200

    implement maestro bootstrap

--- packages/maestro_cli/lib/src/commands/bootstrap_command.dart
+++ packages/maestro_cli/lib/src/commands/bootstrap_command.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
+import 'package:maestro_cli/src/common/constants.dart';
 import 'package:maestro_cli/src/common/logging.dart';
-import 'package:maestro_cli/src/common/paths.dart';
 import 'package:maestro_cli/src/maestro_config.dart';
 
 class BootstrapCommand extends Command<int> {
@@ -11,17 +11,60 @@ class BootstrapCommand extends Command<int> {
 
   @override
   String get description =>
-      'Download artifacts and create default config file.';
+      'Create default config and test files and add maestro as a dev dependency';
 
   @override
   Future<int> run() async {
-    log.info('Downloading artifacts to $artifactsPath ...');
-    await Future<void>.delayed(const Duration(seconds: 1));
+    if (!File('pubspec.yaml').existsSync()) {
+      log.severe(
+        'No pubspec.yaml found. Maestro must be run from Flutter project root.',
+      );
+      return 1;
+    }
 
-    log.info('Writing default config file...');
-    final contents = MaestroConfig.defaultConfig().toToml();
-    File('maestro.toml').writeAsStringSync(contents);
+    _createConfigFile();
+
+    _createDefaultIntegrationTestFile();
+
+    _addMaestroToPubspec();
 
     return 0;
+  }
+}
+
+void _createConfigFile() {
+  log.info('Writing default maestro.toml config file...');
+  final contents = MaestroConfig.defaultConfig().toToml();
+  File('maestro.toml').writeAsStringSync(contents);
+}
+
+void _createDefaultIntegrationTestFile() {
+  log.info('Writing default test_driver/integration_test.dart');
+  final testDriverDir = Directory('test_driver');
+  if (!testDriverDir.existsSync()) {
+    testDriverDir.createSync();
+  }
+
+  final testDriverFile = File('test_driver/integration_test.dart');
+  if (!testDriverFile.existsSync()) {
+    testDriverFile.writeAsStringSync(
+      TestDriverDirectory.defaultTestFileContents,
+    );
+  }
+}
+
+void _addMaestroToPubspec() {
+  log.info('Adding maestro to dev_dependencies in pubspec.yaml');
+
+  final result = Process.runSync(
+    'flutter',
+    ['pub', 'add', 'maestro', '--dev'],
+  );
+
+  if (result.exitCode != 0) {
+    log
+      ..severe('Failed to add maestro to pubspec.yaml.')
+      ..severe(result.stderr);
+    return;
   }
 }
--- packages/maestro_cli/lib/src/common/constants.dart
+++ packages/maestro_cli/lib/src/common/constants.dart
@@ -2,3 +2,24 @@ const configFileName = 'maestro.toml';
 
 /// Version of Maestro CLI.
 const version = '0.0.3';
+
+class TestDriverDirectory {
+  static const defaultTestFileContents = '''
+import 'package:integration_test/integration_test_driver.dart';
+import 'package:maestro/maestro.dart';
+
+// Runs on our machine. Knows nothing about the app being tested.
+Future<void> main() async {
+  print('Waiting for automator server');
+  Automator.init(verbose: true);
+  while (!await Automator.instance.isRunning()) {}
+  print('Automator server is running, starting test drive');
+  try {
+    await integrationDriver();
+  } finally {
+    print('Stopping automator server');
+    await Automator.instance.stop();
+  }
+}
+''';
+}
