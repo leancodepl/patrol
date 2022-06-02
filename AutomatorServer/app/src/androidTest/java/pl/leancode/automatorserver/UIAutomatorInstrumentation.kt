92bcef58005a39b0fb229fe667495de32705ebbc
Bartek Pacia <barpac02@gmail.com>
2022-06-02 13:13:57 +0200

    add openNotifications()

--- AutomatorServer/app/src/androidTest/java/pl/leancode/automatorserver/UIAutomatorInstrumentation.kt
+++ AutomatorServer/app/src/androidTest/java/pl/leancode/automatorserver/UIAutomatorInstrumentation.kt
@@ -5,26 +5,35 @@ import androidx.test.uiautomator.UiDevice
 import com.orhanobut.logger.Logger
 
 class UIAutomatorInstrumentation {
+    private fun getDevice(): UiDevice {
+        return UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    }
+
     fun pressHome() {
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        val device = getDevice();
         Logger.i("Before press home")
         device.pressHome()
         Logger.i("After press home")
     }
 
     fun pressRecentApps() {
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        val device = getDevice()
         Logger.i("Before press recent apps")
         device.pressRecentApps()
         Logger.i("Before press recent apps")
     }
 
     fun pressDoubleRecentApps() {
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        val device = getDevice()
         Logger.i("Before press double recent apps")
         device.pressRecentApps()
         device.pressRecentApps()
         Logger.i("After press double recent apps")
+    }
+
+    fun openNotifications() {
+        val device = getDevice()
+        device.openNotification()
     }
 
     companion object {
