69f2b5b3d9349d480f60b81c4bf13e286352f3c7
Bartek Pacia <barpac02@gmail.com>
2022-06-06 16:27:48 +0200

    Rename `package:maestro` to `:package:maestro_cli` (#52)
    * rename to maestro_test
    
    * update code generated by maestro bootstrap
    
    * fix github actions, update readmes
    
    * add GitHub Action to publish maestro_test

--- packages/maestro_test/README.md
+++ packages/maestro_test/README.md
@@ -1,4 +1,4 @@
-# maestro
+# maestro_test
 
 TODO: Put a short description of the package here that helps potential users
 know whether this package might be useful for them.
--- packages/maestro_test/example/integration_test/app_test.dart
+++ packages/maestro_test/example/integration_test/app_test.dart
@@ -1,42 +1,8 @@
-/// Version of Maestro CLI. Must be kept in sync with pubspec.yaml.
-const version = '0.0.7';
-
-const maestroPackage = 'maestro_test';
-const maestroCliPackage = 'maestro_cli';
-const integrationTestPackage = 'integration_test';
-
-const configFileName = 'maestro.toml';
-
-const driverDirName = 'test_driver';
-const driverFileName = 'integration_test.dart';
-const driverFileContent = '''
-import 'package:integration_test/integration_test_driver.dart';
-import 'package:$maestroPackage/$maestroPackage.dart';
-
-// Runs on the your machine. Knows nothing about the app being tested.
-
-Future<void> main() async {
-  print('Waiting for automator server');
-  Automator.init(verbose: true);
-  while (!await Automator.instance.isRunning()) {}
-  print('Automator server is running, starting test drive');
-  try {
-    await integrationDriver();
-  } finally {
-    print('Stopping automator server');
-    await Automator.instance.stop();
-  }
-}
-''';
-
-const testDirName = 'integration_test';
-const testFileName = 'app_test.dart';
-const testFileContent = '''
 import 'package:example/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:$maestroPackage/$maestroPackage.dart';
+import 'package:maestro_test/maestro_test.dart';
 
 // This is an example file. Use it as a base to create your own Maestro-powered
 // test.
@@ -95,5 +61,3 @@ void main() {
     },
   );
 }
-
-''';
