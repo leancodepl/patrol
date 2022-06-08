91fac21be8a95114f612e57d372fa52b9e00f611
Bartek Pacia <barpac02@gmail.com>
2022-06-08 12:58:24 +0200

    bootstrap: implement GenericTemplate and CounterTemplate

--- packages/maestro_test/example/integration_test/app_test.dart
+++ packages/maestro_test/example/integration_test/app_test.dart
@@ -1,5 +1,4 @@
-import 'package:example/main.dart';
-import 'package:flutter/material.dart';
+import 'package:PROJECT_NAME/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:maestro_test/maestro_test.dart';
@@ -17,43 +16,12 @@ void main() {
   testWidgets(
     'counter state is the same after going to Home and switching apps',
     (tester) async {
-      /// Find the first Text widget whose content is a String which represents
-      /// a num.
-      Text? findCounterText() {
-        final textWidgets = find.byType(Text);
-        final foundElements = textWidgets.evaluate();
-
-        for (final element in foundElements) {
-          final textWidget = element.widget as Text;
-          final text = textWidget.data;
-          if (text == null) {
-            continue;
-          }
-
-          final number = num.tryParse(text);
-          if (number != null) {
-            return textWidget;
-          }
-        }
-
-        return null;
-      }
-
-      await tester.pumpWidget(const MyApp());
+      await tester.pumpWidget(App());
       await tester.pumpAndSettle();
-
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
-      expect(findCounterText()!.data, '1');
 
       await automator.pressHome();
 
       await automator.pressDoubleRecentApps();
-
-      expect(findCounterText()!.data, '1');
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
-      expect(findCounterText()!.data, '2');
 
       await automator.pressHome();
 
