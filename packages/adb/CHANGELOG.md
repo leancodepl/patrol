f3a83700e5c7b714c92d3245af581780711196bc
Bartek Pacia <barpac02@gmail.com>
2022-06-09 12:46:12 +0200

    create package:adb

---
+++ packages/adb/CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1
+
+- Initial release. Move code from `package:maestro_cli`.
---
+++ packages/adb/lib/src/adb.dart
@@ -0,0 +1,144 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:adb/src/exceptions.dart';
+import 'package:adb/src/extensions.dart';
+
+Future<ProcessResult> install(
+  String path, {
+  String? device,
+}) async {
+  final result = await Process.run(
+    'adb',
+    [
+      if (device != null) ...[
+        '-s',
+        device,
+      ],
+      'install',
+      path,
+    ],
+    runInShell: true,
+  );
+
+  if (result.stdErr.isNotEmpty) {
+    if (result.stdErr.contains('INSTALL_FAILED_UPDATE_INCOMPATIBLE')) {
+      throw AdbInstallFailedUpdateIncompatible.fromStdErr(result.stdErr);
+    }
+
+    throw Exception(result.stdErr);
+  }
+
+  return result;
+}
+
+Future<ProcessResult> uninstall(
+  String packageName, {
+  String? device,
+}) async {
+  final result = await Process.run(
+    'adb',
+    [
+      if (device != null) ...[
+        '-s',
+        device,
+      ],
+      'uninstall',
+      packageName,
+    ],
+    runInShell: true,
+  );
+
+  if (result.stdErr.isNotEmpty) {
+    throw Exception(result.stdErr);
+  }
+
+  return result;
+}
+
+/// Sets up port forwarding.
+///
+/// See also:
+///  * https://developer.android.com/studio/command-line/adb#forwardports
+Future<void> forwardPorts({
+  required int fromHost,
+  required int toDevice,
+  String? device,
+  String protocol = 'tcp',
+}) async {
+  final result = await Process.run(
+    'adb',
+    [
+      if (device != null) ...[
+        '-s',
+        device,
+      ],
+      'forward',
+      '$protocol:$fromHost',
+      '$protocol:$toDevice',
+    ],
+    runInShell: true,
+  );
+
+  if (result.stdErr.isNotEmpty) {
+    throw Exception(result.stdErr);
+  }
+}
+
+Future<void> instrument({
+  required String packageName,
+  required String intentClass,
+  String? device,
+  void Function(String)? onStdout,
+  void Function(String)? onStderr,
+}) async {
+  Process process;
+
+  process = await Process.start(
+    'adb',
+    [
+      if (device != null) ...[
+        '-s',
+        device,
+      ],
+      'shell',
+      'am',
+      'instrument',
+      '-w',
+      '$packageName/$intentClass',
+    ],
+  );
+
+  process.stdout.listen((data) {
+    final text = systemEncoding.decode(data);
+    if (onStdout != null) {
+      onStdout(text);
+    }
+  });
+
+  process.stderr.listen((data) {
+    final text = systemEncoding.decode(data);
+    if (onStderr != null) {
+      onStderr(text);
+    }
+  });
+
+  final code = await process.exitCode;
+
+  if (code != 0) {
+    throw Exception('Instrumentation server exited with code $code');
+  }
+}
+
+/// Installs APK from [path].
+///
+/// If the install fails because of INSTALL_FAILED_UPDATE_INCOMPATIBLE, it's
+/// reinstalled.
+Future<void> forceInstallApk(String path, {String? device}) async {
+  try {
+    await install(path, device: device);
+  } on AdbInstallFailedUpdateIncompatible catch (err) {
+    await uninstall(err.packageName, device: device);
+    await install(path, device: device);
+  }
+}
