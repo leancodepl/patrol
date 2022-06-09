6f3422ffc36db94d85c6d56c4fd12a8e17f10c20
Bartek Pacia <barpac02@gmail.com>
2022-06-09 14:18:08 +0200

    add docs to ProcessResult.stdErr extension

--- packages/maestro_cli/lib/src/common/process.dart
+++ packages/maestro_cli/lib/src/common/process.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 
 extension ProcessResultX on ProcessResult {
+  /// A shortcut to avoid typing `as String` every time.
+  ///
+  /// If [stderr] is not a String, this will crash.
   String get stdErr => this.stderr as String;
 }
