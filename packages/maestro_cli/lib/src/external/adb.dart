17c8f60f081b98e1e6354ca242d18d48a102ea19
Bartek Pacia <barpac02@gmail.com>
2022-06-09 12:55:55 +0200

    maestro_cli: use package:adb

--- packages/maestro_cli/lib/src/external/adb.dart
+++ packages/maestro_cli/lib/src/external/adb.dart
@@ -1,46 +1,12 @@
 import 'dart:async';
-import 'dart:io';
 
+import 'package:adb/adb.dart' as adb;
 import 'package:maestro_cli/src/common/common.dart';
-import 'package:path/path.dart' as path;
-
-/// Indicates that adb call failed with INSTALL_FAILED_UPDATE_INCOMPATIBLE.
-class AdbInstallFailedUpdateIncompatible implements Exception {
-  AdbInstallFailedUpdateIncompatible._({
-    required this.packageName,
-    required this.message,
-  });
-
-  factory AdbInstallFailedUpdateIncompatible.fromStdErr(String stderr) {
-    final str = stderr;
-
-    const start = ': Package ';
-    const end = ' signatures do not match';
-
-    final startIndex = str.indexOf(start);
-    final endIndex = str.indexOf(end, startIndex + start.length);
-
-    final packageName = str.substring(startIndex + start.length, endIndex);
-
-    return AdbInstallFailedUpdateIncompatible._(
-      packageName: packageName,
-      message: stderr,
-    );
-  }
-
-  final String message;
-  final String packageName;
-
-  @override
-  String toString() {
-    return 'AdbInstallFailedUpdateIncompatible{packageName: $packageName, message: $message}';
-  }
-}
 
 Future<void> installApps({String? device}) async {
   final progress1 = log.progress('Installing server');
   try {
-    await _forceInstallApk(serverArtifactFile, device: device);
+    await adb.forceInstallApk(serverArtifactFile, device: device);
   } catch (err) {
     progress1.fail('Failed to install server');
     rethrow;
@@ -49,7 +15,7 @@ Future<void> installApps({String? device}) async {
 
   final progress2 = log.progress('Installing instrumentation');
   try {
-    await _forceInstallApk(instrumentationArtifactFile, device: device);
+    await adb.forceInstallApk(instrumentationArtifactFile, device: device);
   } catch (err) {
     progress2.fail('Failed to install instrumentation');
     rethrow;
@@ -61,23 +27,15 @@ Future<void> installApps({String? device}) async {
 Future<void> forwardPorts(int port, {String? device}) async {
   final progress = log.progress('Forwarding ports');
 
-  final result = await Process.run(
-    'adb',
-    [
-      if (device != null) ...[
-        '-s',
-        device,
-      ],
-      'forward',
-      'tcp:$port',
-      'tcp:$port',
-    ],
-    runInShell: true,
-  );
-
-  if (result.stdErr.isNotEmpty) {
+  try {
+    await adb.forwardPorts(
+      fromHost: port,
+      toDevice: port,
+      device: device,
+    );
+  } catch (err) {
     progress.fail('Failed to forward ports');
-    throw Exception(result.stdErr);
+    rethrow;
   }
 
   progress.complete('Forwarded ports');
@@ -86,104 +44,17 @@ Future<void> forwardPorts(int port, {String? device}) async {
 Future<void> runServer({String? device}) async {
   final progress = log.progress('Starting instrumentation server');
 
-  Process process;
   try {
-    process = await Process.start(
-      'adb',
-      [
-        if (device != null) ...[
-          '-s',
-          device,
-        ],
-        'shell',
-        'am',
-        'instrument',
-        '-w',
-        'pl.leancode.automatorserver.test/androidx.test.runner.AndroidJUnitRunner',
-      ],
+    await adb.instrument(
+      packageName: 'pl.leancode.automatorserver.test',
+      intentClass: 'androidx.test.runner.AndroidJUnitRunner',
+      onStdout: log.info,
+      onStderr: log.severe,
     );
   } catch (err) {
     progress.fail('Failed to start instrumentation server');
     rethrow;
   }
 
-  unawaited(
-    process.exitCode.then((code) {
-      final msg = 'Instrumentation server exited with code $code';
-
-      if (code != 0) {
-        log.severe(msg);
-        throw Exception(msg);
-      } else {
-        log.info(msg);
-      }
-    }),
-  );
-
   progress.complete('Started instrumentation server');
-}
-
-/// Installs APK named [name] in [artifactPath].
-///
-/// If the install fails because of INSTALL_FAILED_UPDATE_INCOMPATIBLE, it's
-/// reinstalled.
-Future<void> _forceInstallApk(String name, {required String? device}) async {
-  try {
-    await _installApk(name, device: device);
-  } on AdbInstallFailedUpdateIncompatible catch (err) {
-    await _uninstallApk(err.packageName, device: device);
-    await _installApk(name, device: device);
-  }
-}
-
-Future<ProcessResult> _uninstallApk(
-  String packageName, {
-  required String? device,
-}) async {
-  final result = await Process.run(
-    'adb',
-    [
-      if (device != null) ...[
-        '-s',
-        device,
-      ],
-      'uninstall',
-      packageName,
-    ],
-    runInShell: true,
-  );
-
-  if (result.stdErr.isNotEmpty) {
-    throw Exception(result.stdErr);
-  }
-
-  return result;
-}
-
-Future<ProcessResult> _installApk(
-  String name, {
-  required String? device,
-}) async {
-  final result = await Process.run(
-    'adb',
-    [
-      if (device != null) ...[
-        '-s',
-        device,
-      ],
-      'install',
-      path.join(artifactPath, name),
-    ],
-    runInShell: true,
-  );
-
-  if (result.stdErr.isNotEmpty) {
-    if (result.stdErr.contains('INSTALL_FAILED_UPDATE_INCOMPATIBLE')) {
-      throw AdbInstallFailedUpdateIncompatible.fromStdErr(result.stdErr);
-    }
-
-    throw Exception(result.stdErr);
-  }
-
-  return result;
 }
