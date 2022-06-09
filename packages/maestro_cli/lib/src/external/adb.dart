22e2436bc849d2befc8cb598eb0874d6b8011d43
Bartek Pacia <barpac02@gmail.com>
2022-06-09 12:01:14 +0200

    reinstall apk if certificates don't match

--- packages/maestro_cli/lib/src/external/adb.dart
+++ packages/maestro_cli/lib/src/external/adb.dart
@@ -4,10 +4,43 @@ import 'dart:io';
 import 'package:maestro_cli/src/common/common.dart';
 import 'package:path/path.dart' as path;
 
+/// Indicates that adb call failed with INSTALL_FAILED_UPDATE_INCOMPATIBLE.
+class AdbInstallFailedUpdateIncompatible implements Exception {
+  AdbInstallFailedUpdateIncompatible._({
+    required this.packageName,
+    required this.message,
+  });
+
+  factory AdbInstallFailedUpdateIncompatible.fromStdErr(String stderr) {
+    final str = stderr;
+
+    const start = ': Package ';
+    const end = ' signatures do not match';
+
+    final startIndex = str.indexOf(start);
+    final endIndex = str.indexOf(end, startIndex + start.length);
+
+    final packageName = str.substring(startIndex + start.length, endIndex);
+
+    return AdbInstallFailedUpdateIncompatible._(
+      packageName: packageName,
+      message: stderr,
+    );
+  }
+
+  final String message;
+  final String packageName;
+
+  @override
+  String toString() {
+    return 'AdbInstallFailedUpdateIncompatible{packageName: $packageName, message: $message}';
+  }
+}
+
 Future<void> installApps({String? device}) async {
   final progress1 = log.progress('Installing server');
   try {
-    await _installApk(serverArtifactFile, device: device);
+    await _forceInstallApk(serverArtifactFile, device: device);
   } catch (err) {
     progress1.fail('Failed to install server');
     rethrow;
@@ -16,7 +49,7 @@ Future<void> installApps({String? device}) async {
 
   final progress2 = log.progress('Installing instrumentation');
   try {
-    await _installApk(instrumentationArtifactFile, device: device);
+    await _forceInstallApk(instrumentationArtifactFile, device: device);
   } catch (err) {
     progress2.fail('Failed to install instrumentation');
     rethrow;
@@ -90,7 +123,47 @@ Future<void> runServer({String? device}) async {
   progress.complete('Started instrumentation server');
 }
 
-Future<void> _installApk(String name, {String? device}) async {
+/// Installs APK named [name] in [artifactPath].
+///
+/// If the install fails because of INSTALL_FAILED_UPDATE_INCOMPATIBLE, it's
+/// reinstalled.
+Future<void> _forceInstallApk(String name, {required String? device}) async {
+  try {
+    await _installApk(name, device: device);
+  } on AdbInstallFailedUpdateIncompatible catch (err) {
+    await _uninstallApk(err.packageName, device: device);
+    await _installApk(name, device: device);
+  }
+}
+
+Future<ProcessResult> _uninstallApk(
+  String packageName, {
+  required String? device,
+}) async {
+  final result = await Process.run(
+    'adb',
+    [
+      if (device != null) ...[
+        '-s',
+        device,
+      ],
+      'uninstall',
+      packageName,
+    ],
+    runInShell: true,
+  );
+
+  if (result.stdErr.isNotEmpty) {
+    throw Exception(result.stdErr);
+  }
+
+  return result;
+}
+
+Future<ProcessResult> _installApk(
+  String name, {
+  required String? device,
+}) async {
   final result = await Process.run(
     'adb',
     [
@@ -105,6 +178,12 @@ Future<void> _installApk(String name, {String? device}) async {
   );
 
   if (result.stdErr.isNotEmpty) {
+    if (result.stdErr.contains('INSTALL_FAILED_UPDATE_INCOMPATIBLE')) {
+      throw AdbInstallFailedUpdateIncompatible.fromStdErr(result.stdErr);
+    }
+
     throw Exception(result.stdErr);
   }
+
+  return result;
 }
