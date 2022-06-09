7370beccfb4622e5d39cc9defd3c9077ab670dfb
Bartek Pacia <barpac02@gmail.com>
2022-06-09 13:16:19 +0200

    maestro_cli: move adb.dart from lib/src/external to lib/src/feautures/drive

--- packages/maestro_cli/lib/src/features/drive/drive_command.dart
+++ packages/maestro_cli/lib/src/features/drive/drive_command.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 import 'package:args/command_runner.dart';
 import 'package:maestro_cli/src/common/common.dart';
-import 'package:maestro_cli/src/external/adb.dart' as adb;
 import 'package:maestro_cli/src/external/flutter_driver.dart' as flutter_driver;
+import 'package:maestro_cli/src/features/drive/adb.dart' as adb;
 import 'package:maestro_cli/src/maestro_config.dart';
 
 class DriveCommand extends Command<int> {
