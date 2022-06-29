2de602a60575c6bfa7a39767ee68aaa31f3d0098
Bartek Pacia <barpac02@gmail.com>
2022-06-29 14:53:57 +0200

    start work

---
+++ packages/maestro_test/lib/src/custom_selectors.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+///
+///
+/// There are 3 "type specifiers":
+/// - `.` matches Widget by type.
+///
+///     Example:
+///     ```dart
+///     final textField = $('.TextField');
+///     textField.enterText('user@example.com');
+///     ```
+///
+/// - `#` matches Widget by ValueKey.
+///
+///     Example:
+///     ```dart
+///     final textField = $('#emailTextField');
+///     textField.enterText('user@example.com');
+///     ```
+/// - raw text matches Widget by the text it contains.
+///
+///     Example:
+///     ```dart
+///     final textField = $('Enter email address');
+///     textField.enterText('user@example.com');
+///     ```
+///
+/// ### Chaining
+///
+/// Selectors can be chained, which allows you to write more expressive test
+/// code.
+///
+/// Example:
+///
+///    ```dart
+///     final passwordVisibilityToggle = $('#passwordTextField > .IconButton');
+///     passwordVisibilityToggle.tap();
+///
+Finder $(String expression) {
+  final parts = expression.split(' ');
+
+  final finders = <Finder>[];
+  for (final part in parts) {
+    if (part.alphanumeric) {
+      finders.add(find.text(part));
+      continue;
+    }
+
+    // If part is not alphanumeric, it should have a specifier in the front.
+    final possibleSpecifier = part.substring(0, 1);
+
+    // Match by Widget's class.
+    if (possibleSpecifier == '.') {
+      throw UnimplementedError('Class specifier is not implemented yet');
+    }
+
+    // Match by Widget's key.
+    if (possibleSpecifier == '#') {
+      finders.add(find.byKey(ValueKey(possibleSpecifier)));
+      continue;
+    }
+
+    throw Exception('unknown specifier: $possibleSpecifier');
+  }
+}
+
+extension _StringX on String {
+  bool get alphanumeric {
+    for (final rune in runes) {
+      if (rune < 48 || rune > 90) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+/// Powerful custom selectors extending the default Flutter [Finder].
+extension MaestroFinders on Finder {
+  /// See [$].
+  Finder? $(String expression) {
+    return null;
+  }
+}
