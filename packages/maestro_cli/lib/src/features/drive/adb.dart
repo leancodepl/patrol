35d0398cdf1198fd229bfc6d14d512515e9bee21
Bartek Pacia <barpac02@gmail.com>
2022-07-07 10:27:21 +0200

    fix Adb crashing (#100)

--- packages/maestro_cli/lib/src/features/drive/adb.dart
+++ packages/maestro_cli/lib/src/features/drive/adb.dart
@@ -5,62 +5,73 @@ import 'package:maestro_cli/src/common/common.dart';
 import 'package:maestro_cli/src/features/drive/constants.dart';
 import 'package:path/path.dart' as path;
 
-Future<void> installApps({String? device, bool debug = false}) async {
-  final serverInstallProgress = log.progress('Installing server');
-  try {
-    final p = path.join(
-      artifactPath,
-      debug ? debugServerArtifactFile : serverArtifactFile,
-    );
-    await Adb().forceInstallApk(p, device: device);
-  } catch (err) {
-    serverInstallProgress.fail('Failed to install server');
-    rethrow;
+class MaestroAdb {
+  MaestroAdb() : _adb = Adb();
+
+  final Adb _adb;
+
+  Future<void> init() => _adb.init();
+
+  Future<List<String>> devices() => _adb.devices();
+
+  Future<void> installApps({String? device, bool debug = false}) async {
+    final serverInstallProgress = log.progress('Installing server');
+    try {
+      final p = path.join(
+        artifactPath,
+        debug ? debugServerArtifactFile : serverArtifactFile,
+      );
+      await _adb.forceInstallApk(p, device: device);
+    } catch (err) {
+      serverInstallProgress.fail('Failed to install server');
+      rethrow;
+    }
+    serverInstallProgress.complete('Installed server');
+
+    final instrumentInstallProgress =
+        log.progress('Installing instrumentation');
+    try {
+      final p = path.join(
+        artifactPath,
+        debug ? debugInstrumentationArtifactFile : instrumentationArtifactFile,
+      );
+      await _adb.forceInstallApk(p, device: device);
+    } catch (err) {
+      instrumentInstallProgress.fail('Failed to install instrumentation');
+      rethrow;
+    }
+
+    instrumentInstallProgress.complete('Installed instrumentation');
   }
-  serverInstallProgress.complete('Installed server');
 
-  final instrumentInstallProgress = log.progress('Installing instrumentation');
-  try {
-    final p = path.join(
-      artifactPath,
-      debug ? debugInstrumentationArtifactFile : instrumentationArtifactFile,
-    );
-    await Adb().forceInstallApk(p, device: device);
-  } catch (err) {
-    instrumentInstallProgress.fail('Failed to install instrumentation');
-    rethrow;
+  Future<void> forwardPorts(int port, {String? device}) async {
+    final progress = log.progress('Forwarding ports');
+
+    try {
+      await _adb.forwardPorts(
+        fromHost: port,
+        toDevice: port,
+        device: device,
+      );
+    } catch (err) {
+      progress.fail('Failed to forward ports');
+      rethrow;
+    }
+
+    progress.complete('Forwarded ports');
   }
 
-  instrumentInstallProgress.complete('Installed instrumentation');
-}
-
-Future<void> forwardPorts(int port, {String? device}) async {
-  final progress = log.progress('Forwarding ports');
-
-  try {
-    await Adb().forwardPorts(
-      fromHost: port,
-      toDevice: port,
+  void runServer({
+    required String? device,
+    required int port,
+  }) {
+    _adb.instrument(
+      packageName: 'pl.leancode.automatorserver.test',
+      intentClass: 'androidx.test.runner.AndroidJUnitRunner',
       device: device,
+      onStdout: log.info,
+      onStderr: log.severe,
+      arguments: {envPortKey: port.toString()},
     );
-  } catch (err) {
-    progress.fail('Failed to forward ports');
-    rethrow;
   }
-
-  progress.complete('Forwarded ports');
-}
-
-void runServer({
-  required String? device,
-  required int port,
-}) {
-  Adb().instrument(
-    packageName: 'pl.leancode.automatorserver.test',
-    intentClass: 'androidx.test.runner.AndroidJUnitRunner',
-    device: device,
-    onStdout: log.info,
-    onStderr: log.severe,
-    arguments: {envPortKey: port.toString()},
-  );
 }
