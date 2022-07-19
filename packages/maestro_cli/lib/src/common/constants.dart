82c19b3930cef5a0c8f287a4d80bfb3db16e14f1
Bartek Pacia <barpac02@gmail.com>
2022-07-19 12:32:51 +0200

    `maestro_cli` v0.3.4, `maestro_test` 0.3.3 (#112)
    * maestro_test: set version to 0.3.3
    
    * maestro_cli: set version to 0.3.4
    
    * maestro_cli-publish workflow: upload to pub.dev at the end

--- packages/maestro_cli/lib/src/common/constants.dart
+++ packages/maestro_cli/lib/src/common/constants.dart
@@ -1,5 +1,5 @@
 /// Version of Maestro CLI. Must be kept in sync with pubspec.yaml.
-const version = '0.3.3';
+const version = '0.3.4';
 
 const maestroPackage = 'maestro_test';
 const maestroCliPackage = 'maestro_cli';
