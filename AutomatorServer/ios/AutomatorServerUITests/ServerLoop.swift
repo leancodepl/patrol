f689fe952abc873c9a0d7553a786992b6e131657
Bartek Pacia <barpac02@gmail.com>
2022-07-26 16:37:24 +0200

    fix Xcode project, again. God.

---
+++ AutomatorServer/ios/AutomatorServerUITests/ServerLoop.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+class ServerLoop: XCTestCase {
+    func testRunMaestroServer() {
+        print("Starting server...")
+        
+        let maestroServer = MaestroServer()
+        do {
+            try maestroServer.start()
+            print("Server started")
+        } catch let error {
+            print("error: \(error)")
+            maestroServer.stop()
+        }
+    }
+}
