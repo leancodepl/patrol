151fde986efc706d8721d1a442359d359553ac15
Bartek Pacia <barpac02@gmail.com>
2022-07-26 13:01:50 +0200

    remove Pods from git

--- AutomatorServer/ios/Pods/Target Support Files/Embassy/Embassy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Embassy : NSObject
-@end
-@implementation PodsDummy_Embassy
-@end
--- AutomatorServer/ios/Pods/Target Support Files/Pods-AutomatorServerUITests/Pods-AutomatorServerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AutomatorServerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AutomatorServerUITests
-@end
