50405556521393176a248e5c3e8071be102c91fc
Bartek Pacia <barpac02@gmail.com>
2022-08-12 13:08:20 +0200

    remove commented-out code in ServerLoop.swift

--- AutomatorServer/ios/AutomatorServerUITests/ServerLoop.swift
+++ AutomatorServer/ios/AutomatorServerUITests/ServerLoop.swift
@@ -5,34 +5,13 @@ class ServerLoop: XCTestCase {
     Logger.shared.i("Starting server loop...")
 
     
-    let maestroServer: MaestroServer
-    do {
-      maestroServer = try MaestroServer()
-    } catch let err {
-      throw err
-    }
+    let maestroServer = try MaestroServer()
 
-    setUpInterruptionMonitor()
-    
     do {
       try maestroServer.start()
     } catch let err {
       Logger.shared.e("Failed to start server: \(err)")
       maestroServer.stop()
     }
-  }
-
-  func setUpInterruptionMonitor() {
-    //    addUIInterruptionMonitor(withDescription: "Access to sound recording") { (alert) -> Bool in
-    //      Logger.shared.i("interruption caught")
-    //      if alert.staticTexts["Odkryj Rudy would like to find and connect to devices on your local network."]
-    //        .exists
-    //      {
-    //        alert.buttons["OK"].tap()
-    //      } else {
-    //        alert.buttons["Don’t Allow"].tap()
-    //      }
-    //      return true
-    //    }
   }
 }
