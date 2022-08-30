2c3f8b51c1d2dffa805cdc492f595d822ef5da08
Bartek Pacia <barpac02@gmail.com>
2022-08-30 16:26:09 +0200

    remove empry `maestro_tester_test.dart`

--- packages/maestro_test/test/maestro_tester_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:maestro_test/src/custom_finders/custom_finders.dart';
-
-void main() {
-  group('dragging', () {
-    maestroTest('until visible', ($) async {});
-  });
-}
