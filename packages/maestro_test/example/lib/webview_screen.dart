c92e5c4cd1f515c86a4431f766ec00256757c28d
Bartek Pacia <barpac02@gmail.com>
2022-08-31 23:58:00 +0200

    one tiny super parameter

--- packages/maestro_test/example/lib/webview_screen.dart
+++ packages/maestro_test/example/lib/webview_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatelessWidget {
-  const WebViewScreen({Key? key}) : super(key: key);
+  const WebViewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
