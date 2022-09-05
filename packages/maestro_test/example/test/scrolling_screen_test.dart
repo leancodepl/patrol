cd36ab18162b97984ab1016a09aace6714500022
Bartek Pacia <barpac02@gmail.com>
2022-09-05 19:51:57 +0200

    Fix `scrollTo` not scrolling in some weird cases (#237)
    * fixed, but why it works like that, don't ask me
    
    * remove debugging aids
    
    * use global consts for `delta` and `maxIteration`

--- packages/maestro_test/example/test/scrolling_screen_test.dart
+++ packages/maestro_test/example/test/scrolling_screen_test.dart
@@ -2,13 +2,10 @@ import 'package:example/keys.dart';
 import 'package:example/main.dart';
 import 'package:maestro_test/maestro_test.dart';
 
-// Idea: rename MaestroFinder.visible to MaestroFinder.waitForVisible
-// Idea: create bool getter MaestroFinder.visible
-
 void main() {
   maestroTest('drags to a widget', ($) async {
     await $.pumpWidgetAndSettle(ExampleApp());
-    await $('Open scrolling screen').tap();
+    await $('Open scrolling screen').scrollTo().tap();
 
     expect(await $(K.topText).waitUntilVisible(), findsOneWidget);
     expect($(K.bottomText).hitTestable(), findsNothing);
