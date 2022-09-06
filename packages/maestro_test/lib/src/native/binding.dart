b9c81cf2a0276e7630391dbc0f6ede5895d0fae5
Bartek Pacia <barpac02@gmail.com>
2022-09-06 11:30:08 +0200

    Add tests for webview, create `MaestroBinding` (#239)
    * create `MaestroBinding`
    
    * example app: add a sample webview screen
    
    * example app: write integration test for the sample webview screen
    
    * use `MaestroBinding` in `Maestro.forTest`
    
    * example app: set minSDK to 21 and targetSDK to 31
    
    * example app: create an integration test that does nothing but wait
    
    * example app: `webview_test.dart`: improve test description

---
+++ packages/maestro_test/lib/src/native/binding.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+/// Binding that enables some of Maestro's custom functionality, such as tapping
+/// on WebViews during a test.
+class MaestroBinding extends IntegrationTestWidgetsFlutterBinding {
+  /// Default constructor that only calls the superclass constructor.
+  MaestroBinding() : super();
+
+  /// Returns an instance of the [MaestroBinding], creating and initializing it
+  /// if necessary.
+  factory MaestroBinding.ensureInitialized() {
+    if (_instance == null) {
+      MaestroBinding();
+    }
+    return _instance!;
+  }
+
+  // TODO: Remove once https://github.com/flutter/flutter/pull/108430 is
+  // available on the stable channel
+  @override
+  TestBindingEventSource get pointerEventSource => TestBindingEventSource.test;
+
+  @override
+  void initInstances() {
+    super.initInstances();
+    _instance = this;
+  }
+
+  /// The singleton instance of this object.
+  ///
+  /// Provides access to the features exposed by this class. The binding must be
+  /// initialized before using this getter; this is typically done by calling
+  /// [MaestroBinding.ensureInitialized].
+  static MaestroBinding get instance => BindingBase.checkInstance(_instance);
+  static MaestroBinding? _instance;
+}
