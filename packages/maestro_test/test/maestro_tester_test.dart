81cd8083ef684334dde40ed378aa822c349aa485
Bartek Pacia <barpac02@gmail.com>
2022-09-08 14:04:43 +0200

    write unit tests for `dragUntilVisible`

--- packages/maestro_test/test/maestro_tester_test.dart
+++ packages/maestro_test/test/maestro_tester_test.dart
@@ -386,6 +386,182 @@ void main() {
     );
   });
 
+  group('dragUntilVisible', () {
+    maestroTest(
+      'throws exception when no Scrollable is found within timeout',
+      (tester) async {
+        await tester.pumpWidget(const MaterialApp());
+
+        await expectLater(
+          () => tester.dragUntilVisible(
+            finder: find.text('text'),
+            view: find.byType(Scrollable),
+            moveStep: const Offset(0, 16),
+          ),
+          // because it was waiting for a Scrollable to appear
+          throwsA(isA<WaitUntilVisibleTimeoutException>()),
+        );
+      },
+    );
+
+    maestroTest(
+      'throws StateError when no widget is found after reaching maxIteration',
+      (tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: ListView(
+              children: const [Text('one'), Text('two')],
+            ),
+          ),
+        );
+
+        await expectLater(
+          () => tester.dragUntilVisible(
+            finder: find.text('three'),
+            view: find.byType(Scrollable),
+            moveStep: const Offset(0, 16),
+          ),
+          throwsStateError,
+        );
+      },
+    );
+
+    maestroTest('drags to existing and visible widget', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: SingleChildScrollView(
+            child: Column(
+              children: const [Text('some text')],
+            ),
+          ),
+        ),
+      );
+
+      expect(find.text('some text').hitTestable(), findsOneWidget);
+
+      await tester.dragUntilVisible(
+        finder: find.text('some text'),
+        view: find.byType(Scrollable),
+        moveStep: const Offset(0, 16),
+      );
+
+      expect(find.text('some text').hitTestable(), findsOneWidget);
+    });
+
+    maestroTest(
+      'drags to existing but not visible widget in Scrollable appearing late',
+      (tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: FutureBuilder(
+              future: Future<void>.delayed(const Duration(milliseconds: 300)),
+              builder: (context, snapshot) {
+                return LayoutBuilder(
+                  builder: (_, constraints) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      return const CircularProgressIndicator();
+                    }
+
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const Text('top text'),
+                          SizedBox(height: constraints.maxHeight * 2),
+                          const Text('bottom text'),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        );
+
+        expect(find.text('top text').hitTestable(), findsNothing);
+        expect(find.text('bottom text').hitTestable(), findsNothing);
+
+        await tester.dragUntilVisible(
+          finder: find.text('top text'),
+          view: find.byType(Scrollable),
+          moveStep: const Offset(0, 16),
+        );
+        final initialScrollPosition = tester.tester
+            .firstWidget<Scrollable>(find.byType(Scrollable))
+            .controller
+            ?.position
+            .pixels;
+
+        await tester.dragUntilVisible(
+          finder: find.text('bottom text'),
+          view: find.byType(Scrollable),
+          moveStep: const Offset(0, 16),
+        );
+
+        final finalScrollPosition = tester.tester
+            .firstWidget<Scrollable>(find.byType(Scrollable))
+            .controller
+            ?.position
+            .pixels;
+
+        expect(find.text('top text').hitTestable(), findsNothing);
+        expect(find.text('bottom text').hitTestable(), findsOneWidget);
+        expect(initialScrollPosition, isZero);
+        expect(finalScrollPosition, isPositive);
+      },
+    );
+
+    maestroTest(
+      'drags to existing and visible widget in the first Scrollable',
+      (tester) async {
+        await tester.pumpWidget(
+          MaterialApp(
+            home: Column(
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    children: const [
+                      Text('text 1'),
+                      Text('text 1'),
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    children: const [Text('text 2')],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+
+        expect(find.text('text 1').hitTestable(), findsNWidgets(2));
+        expect(find.text('text 2').hitTestable(), findsOneWidget);
+
+        await tester.dragUntilVisible(
+          finder: find.text('text 1'),
+          view: find.byType(Scrollable),
+          moveStep: const Offset(0, 16),
+        );
+
+        expect(find.text('text 1').hitTestable(), findsNWidgets(2));
+        expect(find.text('text 2').hitTestable(), findsOneWidget);
+
+        await tester.dragUntilVisible(
+          finder: find.text('text 2'),
+          view: find.byType(Scrollable),
+          moveStep: const Offset(0, 16),
+        );
+
+        expect(find.text('text 1').hitTestable(), findsNWidgets(2));
+        expect(find.text('text 2').hitTestable(), findsOneWidget);
+      },
+    );
+  });
+
   group('scrollUntilExists', () {
     maestroTest(
       'throws exception when no Scrollable is found within timeout',
