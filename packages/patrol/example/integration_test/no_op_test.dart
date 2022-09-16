7a4ee0c9bbe49d773eee77128d98f48c015e935e
Bartek Pacia <barpac02@gmail.com>
2022-09-17 00:43:31 +0200

    Print more comprehensible error message when native action fails on Android (#295)

--- packages/patrol/example/integration_test/no_op_test.dart
+++ packages/patrol/example/integration_test/no_op_test.dart
@@ -4,13 +4,18 @@ import 'package:patrol/patrol.dart';
 import 'config.dart';
 
 Future<void> main() async {
-  Patrol.forTest();
+  final patrol = Patrol.forTest();
 
   patrolTest(
     'does nothing',
     config: patrolConfig,
     ($) async {
       await $.pumpWidgetAndSettle(const ExampleApp());
+
+      await patrol.pressHome();
+      await patrol.pressDoubleRecentApps();
+
+      await patrol.tap(const Selector(text: 'Some strange button'));
 
       await Future<void>.delayed(const Duration(minutes: 10));
     },
