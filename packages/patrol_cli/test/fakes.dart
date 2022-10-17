1effe4f072b2abe83e53fd1cb681cf9e270e2508
Bartek Pacia <barpac02@gmail.com>
2022-10-17 11:07:58 +0200

    Set correct exit code of Patrol CLI and test it (#458)
    * set exit code 1 when any test fails
    
    * write tests for DriveCommand exit code (failing)
    
    * inject AndroidDriver and IOSDriver into DriveCommand
    
    * inject FlutterDriver into DriverCommand, make all tests pass

--- packages/patrol_cli/test/fakes.dart
+++ packages/patrol_cli/test/fakes.dart
@@ -1,6 +1,10 @@
 import 'package:mocktail/mocktail.dart';
+import 'package:patrol_cli/src/features/drive/device.dart';
+
+class FakeDevice extends Fake implements Device {}
 
 void setUpFakes() {
   registerFallbackValue(Uri());
   registerFallbackValue(<int>[]);
+  registerFallbackValue(FakeDevice());
 }
