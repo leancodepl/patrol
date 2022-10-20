f90c0b2b6206111b8f7db6d457275851e124bc28
Bartek Pacia <barpac02@gmail.com>
2022-10-20 20:38:28 +0200

    create `airplane_mode_test.dart`

---
+++ packages/patrol/example/integration_test/features/services/airplane_mode_test.dart
@@ -0,0 +1,20 @@
+import 'package:example/main.dart';
+import 'package:patrol/patrol.dart';
+
+import '../../config.dart';
+
+void main() {
+  patrolTest(
+    'disables and enables airplane mode twice',
+    config: patrolConfig,
+    nativeAutomation: true,
+    ($) async {
+      await $.pumpWidgetAndSettle(ExampleApp());
+
+      await $.native.disableAirplaneMode();
+      await $.native.enableAirplaneMode();
+      await $.native.disableAirplaneMode();
+      await $.native.enableAirplaneMode();
+    },
+  );
+}
