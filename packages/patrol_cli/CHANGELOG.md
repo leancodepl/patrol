203dd3a96266d56a18870d5a00478f4b67d62da9
Bartek Pacia <barpac02@gmail.com>
2022-10-24 17:52:46 +0200

    patrol_cli: bump version to 0.7.3+1 (#506)

--- packages/patrol_cli/CHANGELOG.md
+++ packages/patrol_cli/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.7.3+1
+
+- Disable terminal animations to fix logs on the CI (#498)
+
 ## 0.7.3
 
 - AutomatorServer:
--- packages/patrol_cli/lib/src/common/constants.dart
+++ packages/patrol_cli/lib/src/common/constants.dart
@@ -1,7 +1,7 @@
 import 'package:path/path.dart' as path;
 
 /// Version of Patrol CLI. Must be kept in sync with pubspec.yaml.
-const globalVersion = '0.7.3';
+const globalVersion = '0.7.3+1';
 
 const patrolPackage = 'patrol';
 const patrolCliPackage = 'patrol_cli';
