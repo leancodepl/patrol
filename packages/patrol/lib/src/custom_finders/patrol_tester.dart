be99fc5797a5124608989cf2b7d8f6333c900f47
Bartek Pacia <barpac02@gmail.com>
2022-10-24 12:22:16 +0200

    add `andSettle` to `PatrolTester.scrollUntilExists()`

--- packages/patrol/lib/src/custom_finders/patrol_tester.dart
+++ packages/patrol/lib/src/custom_finders/patrol_tester.dart
@@ -401,6 +401,7 @@ class PatrolTester {
     double delta = defaultScrollDelta,
     int maxScrolls = defaultScrollMaxIteration,
     Duration duration = const Duration(milliseconds: 50),
+    bool? andSettle,
   }) async {
     assert(maxScrolls > 0, 'maxScrolls must be positive number');
     scrollable ??= find.byType(Scrollable);
@@ -433,6 +434,7 @@ class PatrolTester {
         moveStep: moveStep,
         maxIteration: maxScrolls,
         duration: duration,
+        andSettle: andSettle,
       );
 
       return resolvedFinder;
