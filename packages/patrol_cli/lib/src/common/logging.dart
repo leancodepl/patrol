8bba9ccd8b5fcb2e49a1a84d4481b1c08238e433
Bartek Pacia <barpac02@gmail.com>
2022-10-26 01:10:07 +0200

    Revert "stop using `Progress` from `package:mason_logger` (#498)"
    This reverts commit 1a1dc81320230891b0ea88f103885b652d1bca96.

--- packages/patrol_cli/lib/src/common/logging.dart
+++ packages/patrol_cli/lib/src/common/logging.dart
@@ -6,39 +6,12 @@ import 'package:ansi_styles/ansi_styles.dart';
 import 'package:logging/logging.dart';
 import 'package:mason_logger/mason_logger.dart' as mason_logger;
 
-// Workaround for https://github.com/felangel/mason/issues/527.
-class Progress implements mason_logger.Progress {
-  Progress._({required this.logger, required String firstMessage}) {
-    logger.info('$firstMessage...');
-  }
-
-  final Logger logger;
-
-  @override
-  void cancel() {
-    throw UnimplementedError();
-  }
-
-  @override
-  void complete([String? update]) {
-    logger.info(update);
-  }
-
-  @override
-  void fail([String? update]) {
-    logger.severe(update);
-  }
-
-  @override
-  void update(String update) {
-    logger.info(update);
-  }
-}
-
 extension LoggerX on Logger {
+  static final _logger = mason_logger.Logger();
+
   /// Writes progress message to stdout.
-  Progress progress(String message) {
-    return Progress._(firstMessage: message, logger: this);
+  mason_logger.Progress progress(String message) {
+    return _logger.progress(message);
   }
 
   set verbose(bool newValue) => _verbose = newValue;
