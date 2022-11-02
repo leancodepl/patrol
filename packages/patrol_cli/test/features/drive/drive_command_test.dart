58e63b680a96c6705fe6d87d5e2e6a12b5634464
Bartek Pacia <barpac02@gmail.com>
2022-11-02 20:59:07 +0100

    fix tests

--- packages/patrol_cli/test/features/drive/drive_command_test.dart
+++ packages/patrol_cli/test/features/drive/drive_command_test.dart
@@ -77,6 +77,7 @@ void main() {
 
       final iosDriver = MockIOSDriver();
       flutterDriver = MockFlutterDriver();
+      when(() => flutterDriver.build(any(), any())).thenAnswer((_) async {});
 
       driveCommand = DriveCommand(
         parentDisposeScope: parentDisposeScope,
