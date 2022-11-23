a2f409467105fa123fa0e81ef0afd85cc239ff35
Bartek Pacia <barpac02@gmail.com>
2022-11-23 11:51:35 +0100

    add expect()s

--- packages/patrol/example/integration_test/process_test.dart
+++ packages/patrol/example/integration_test/process_test.dart
@@ -1,6 +1,6 @@
 @Tags(['android', 'ios'])
 
-import 'dart:io' show Platform;
+import 'dart:io' as io;
 
 import 'package:example/main.dart';
 import 'package:flutter/material.dart';
@@ -10,49 +10,43 @@ import 'package:patrol/patrol.dart';
 import 'config.dart';
 
 Future<void> main() async {
-  late String mapsId;
-  if (Platform.isIOS) {
-    mapsId = 'com.apple.Maps';
-  } else if (Platform.isAndroid) {
-    mapsId = 'com.google.android.apps.maps';
-  }
-
   patrolTest(
     'takes a few screenshots',
     config: patrolConfig,
     nativeAutomation: true,
     ($) async {
-      await $.host.runProcess(
+      io.ProcessResult? result;
+
+      result = await $.host.runProcess(
         'bash',
         arguments: ['-c', 'echo "hello world" > hello.txt'],
       );
+      expect(result.exitCode, 0);
 
       await $.pumpWidgetAndSettle(ExampleApp());
 
       expect($(#counterText).text, '0');
 
-      await $.host.runProcess(
+      result = await $.host.runProcess(
         'bash',
         arguments: [
           '-c',
           'echo "counter text: ${$(#counterText).text}" > counter_text.txt',
         ],
       );
+      expect(result.exitCode, 0);
 
       await $(FloatingActionButton).tap();
 
       await $.native.pressHome();
 
-      await $.native.openApp(appId: mapsId);
-
-      await $.native.pressHome();
-
       await $.native.openApp();
 
-      await $.host.runProcess(
+      result = await $.host.runProcess(
         'bash',
         arguments: ['-c', '"this command will fail"'],
       );
+      expect(result.exitCode, 127);
 
       expect($(#counterText).text, '1');
     },
