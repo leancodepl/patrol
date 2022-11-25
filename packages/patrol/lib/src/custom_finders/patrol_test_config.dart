0ab91cc52af762328ff0ca436e12f663d367854c
Bartek Pacia <barpac02@gmail.com>
2022-11-25 12:20:12 +0100

    PatrolTestConfig: stop using package:freezed

--- packages/patrol/lib/src/custom_finders/patrol_test_config.dart
+++ packages/patrol/lib/src/custom_finders/patrol_test_config.dart
@@ -1,48 +1,75 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'patrol_finder.dart';
 import 'patrol_tester.dart';
 
-part 'patrol_test_config.freezed.dart';
-
 /// Common configuration for [PatrolTester] and [PatrolFinder].
-@freezed
-class PatrolTestConfig with _$PatrolTestConfig {
+class PatrolTestConfig {
   /// Creates a new [PatrolTestConfig].
-  const factory PatrolTestConfig({
-    /// Time after which [PatrolFinder.waitUntilExists] fails if it doesn't
-    /// finds a widget.
-    @Default(Duration(seconds: 10)) Duration existsTimeout,
+  const PatrolTestConfig({
+    this.existsTimeout = const Duration(seconds: 10),
+    this.visibleTimeout = const Duration(seconds: 10),
+    this.settleTimeout = const Duration(seconds: 10),
+    this.andSettle = true,
+    this.appName,
+    this.packageName,
+    this.bundleId,
+  });
 
-    /// Time after which [PatrolFinder.waitUntilVisible] fails if it doesn't
-    /// finds a widget.
-    ///
-    /// [PatrolFinder.waitUntilVisible] is used internally by [PatrolFinder.tap]
-    /// and [PatrolFinder.enterText].
-    @Default(Duration(seconds: 10)) Duration visibleTimeout,
+  /// Time after which [PatrolFinder.waitUntilExists] fails if it doesn't finds
+  /// a widget.
+  final Duration existsTimeout;
 
-    /// Time after which [PatrolTester.pumpAndSettle] fails.
-    @Default(Duration(seconds: 10)) Duration settleTimeout,
+  /// Time after which [PatrolFinder.waitUntilVisible] fails if it doesn't finds
+  /// a widget.
+  ///
+  /// [PatrolFinder.waitUntilVisible] is used internally by [PatrolFinder.tap]
+  /// and [PatrolFinder.enterText].
+  ///
+  final Duration visibleTimeout;
 
-    /// Whether to call [WidgetTester.pumpAndSettle] after actions such as
-    /// [PatrolFinder.tap] and [PatrolFinder]. If false, only
-    /// [WidgetTester.pump] is called.
-    @Default(true) bool andSettle,
+  /// Time after which [PatrolTester.pumpAndSettle] fails.
+  final Duration settleTimeout;
 
-    /// Name of the application under test.
-    ///
-    /// Used in [PatrolTester.log].
+  /// Whether to call [WidgetTester.pumpAndSettle] after actions such as
+  /// [PatrolFinder.tap] and [PatrolFinder]. If false, only [WidgetTester.pump]
+  /// is called.
+  final bool andSettle;
+
+  /// Name of the application under test.
+  ///
+  /// Used in [PatrolTester.log].
+  final String? appName;
+
+  /// Package name of the application under test.
+  ///
+  /// Android only.
+  final String? packageName;
+
+  /// Bundle identifier name of the application under test.
+  ///
+  /// iOS only.
+  final String? bundleId;
+
+  /// Creates a copy of this config but with the given fields replaced with
+  /// the new values.
+  PatrolTestConfig copyWith({
+    Duration? existsTimeout,
+    Duration? visibleTimeout,
+    Duration? settleTimeout,
+    bool? andSettle,
     String? appName,
-
-    /// Package name of the application under test.
-    ///
-    /// Android only.
     String? packageName,
-
-    /// Bundle identifier name of the application under test.
-    ///
-    /// iOS only.
     String? bundleId,
-  }) = _PatrolTestConfig;
+  }) {
+    return PatrolTestConfig(
+      existsTimeout: existsTimeout ?? this.existsTimeout,
+      visibleTimeout: visibleTimeout ?? this.visibleTimeout,
+      settleTimeout: settleTimeout ?? this.settleTimeout,
+      andSettle: andSettle ?? this.andSettle,
+      appName: appName ?? this.appName,
+      packageName: packageName ?? this.packageName,
+      bundleId: bundleId ?? this.bundleId,
+    );
+  }
 }
