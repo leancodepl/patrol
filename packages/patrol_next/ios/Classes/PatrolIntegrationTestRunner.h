a8b554848e33de53a02634fc39c9404d4218f0b0
Bartek Pacia <barpac02@gmail.com>
2022-12-09 14:12:45 +0100

    create `PATROL_INTEGRATION_TEST_IOS_RUNNER` macro

--- packages/patrol_next/ios/Classes/PatrolIntegrationTestRunner.h
+++ packages/patrol_next/ios/Classes/PatrolIntegrationTestRunner.h
@@ -1,4 +1,5 @@
 @import Foundation;
+@import ObjectiveC.runtime;
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -29,5 +30,70 @@ typedef void (^PatrolIntegrationTestResults)(SEL nativeTestSelector, BOOL succes
 + (NSString *)testCaseNameFromDartTestName:(NSString *)dartTestName;
 
 @end
+
+/// Adapted from:
+/// https://github.com/flutter/flutter/blob/master/packages/integration_test/ios/Classes/IntegrationTestIosTest.h
+#define PATROL_INTEGRATION_TEST_IOS_RUNNER(__test_class)                                                                     \
+  @interface RunnerTests : XCTestCase                                                                                        \
+  @end                                                                                                                       \
+                                                                                                                             \
+  @implementation RunnerTests                                                                                                \
+                                                                                                                             \
+  +(NSArray<NSInvocation *> *)testInvocations {                                                                              \
+    /* Start native automation gRPC server */                                                                                \
+    PatrolServer *server = [[PatrolServer alloc] init];                                                                      \
+    [server startWithCompletionHandler:^(NSError * err) {                                                                    \
+      NSLog(@"Server loop done, error: %@", err);                                                                            \
+    }];                                                                                                                      \
+                                                                                                                             \
+    XCUIApplication *app = [[XCUIApplication alloc] init];                                                                   \
+    [app launch];                                                                                                            \
+                                                                                                                             \
+    /* Spin the runloop waiting for test results */                                                                          \
+    while (!server.dartTestResults) {                                                                                        \
+      [NSRunLoop.currentRunLoop runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];                                     \
+    }                                                                                                                        \
+                                                                                                                             \
+    PatrolIntegrationTestRunner *testRunner = [[PatrolIntegrationTestRunner alloc] init];                                    \
+    NSMutableArray<NSInvocation *> *testInvocations = [[NSMutableArray alloc] init];                                         \
+                                                                                                                             \
+    /* Dynamically create test case methods from Dart results */                                                             \
+    [testRunner iterateOverTestResults:server.dartTestResults                                                                \
+                          withSelector:^(SEL testSelector, BOOL success, NSString * failureMessage) {                        \
+                            IMP assertImplementation = imp_implementationWithBlock(^(id _self) {                             \
+                              XCTAssertTrue(success, @"%@", failureMessage);                                                 \
+                            });                                                                                              \
+                            class_addMethod(self, testSelector, assertImplementation, "v@:");                                \
+                            NSMethodSignature *signature = [self instanceMethodSignatureForSelector:testSelector];           \
+                            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];               \
+                            invocation.selector = testSelector;                                                              \
+                            [testInvocations addObject:invocation];                                                          \
+                          }];                                                                                                \
+                                                                                                                             \
+    /* Add captured screenshots */                                                                                           \
+    NSDictionary<NSString *, UIImage *> *capturedScreenshotsByName = testRunner.capturedScreenshotsByName;                   \
+    if (capturedScreenshotsByName.count > 0) {                                                                               \
+      IMP screenshotImplementation = imp_implementationWithBlock(^(id _self) {                                               \
+        [capturedScreenshotsByName enumerateKeysAndObjectsUsingBlock:^(NSString * name, UIImage * screenshot, BOOL * stop) { \
+          XCTAttachment *attachment = [XCTAttachment attachmentWithImage:screenshot];                                        \
+          attachment.lifetime = XCTAttachmentLifetimeKeepAlways;                                                             \
+          if (name != nil) {                                                                                                 \
+            attachment.name = name;                                                                                          \
+          }                                                                                                                  \
+          [_self addAttachment:attachment];                                                                                  \
+        }];                                                                                                                  \
+      });                                                                                                                    \
+      SEL attachmentSelector = NSSelectorFromString(@"screenshotPlaceholder");                                               \
+      class_addMethod(self, attachmentSelector, screenshotImplementation, "v@:");                                            \
+      NSMethodSignature *attachmentSignature = [self instanceMethodSignatureForSelector:attachmentSelector];                 \
+      NSInvocation *attachmentInvocation = [NSInvocation invocationWithMethodSignature:attachmentSignature];                 \
+      attachmentInvocation.selector = attachmentSelector;                                                                    \
+      [testInvocations addObject:attachmentInvocation];                                                                      \
+    }                                                                                                                        \
+                                                                                                                             \
+    return testInvocations;                                                                                                  \
+  }                                                                                                                          \
+                                                                                                                             \
+  @end                                                                                                                       \
 
 NS_ASSUME_NONNULL_END
