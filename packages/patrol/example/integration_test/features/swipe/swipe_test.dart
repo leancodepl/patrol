2b1ea541530b30e6c3f93b278259cd0a7afea46b
Bartek Pacia <barpac02@gmail.com>
2022-12-12 14:43:49 +0100

    (re)add native swipe

---
+++ packages/patrol/example/integration_test/features/swipe/swipe_test.dart
@@ -0,0 +1,29 @@
+@Tags(['android'])
+
+import 'dart:io' show Platform;
+
+import '../../common.dart';
+
+Future<void> main() async {
+  late String appId;
+  if (Platform.isIOS) {
+    appId = 'com.apple.Preferences';
+  } else if (Platform.isAndroid) {
+    appId = 'com.android.settings';
+  }
+
+  patrol('scrolls the Settings app', ($) async {
+    await $.pumpWidgetAndSettle(ExampleApp());
+
+    final window = $.tester.binding.window;
+    final width = window.physicalSize.width;
+    final height = window.physicalSize.height;
+
+    await $.native.openApp(appId: appId);
+
+    final from = Offset((width / 2) / width, (height / 2) / height);
+    final to = Offset((width / 2) / width, 100 / height);
+
+    await $.native.swipe(from: from, to: to);
+  });
+}
