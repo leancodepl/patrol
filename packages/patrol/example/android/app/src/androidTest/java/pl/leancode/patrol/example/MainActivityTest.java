b18b1eb3ebc20aa487f0172fb1d18c8db126ef68
Bartek Pacia <bartek.pacia@leancode.pl>
2023-01-12 13:39:38 +0100

    example app: use PatrolTestRunner instead of FlutterTestRunner

--- packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
+++ packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
@@ -1,11 +1,11 @@
 package pl.leancode.patrol.example;
 
-import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 import pl.leancode.patrol.PatrolTestRule;
+import pl.leancode.patrol.PatrolTestRunner;
 
-@RunWith(FlutterTestRunner.class)
+@RunWith(PatrolTestRunner.class)
 public class MainActivityTest {
     @Rule
     public PatrolTestRule<MainActivity> rule = new PatrolTestRule<>(MainActivity.class);
