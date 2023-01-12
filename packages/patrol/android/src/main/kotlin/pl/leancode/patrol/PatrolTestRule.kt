6f56904412a6cf89bb0e4aba5a6ebf0ba70c4506
Bartek Pacia <bartek.pacia@leancode.pl>
2023-01-12 13:42:06 +0100

    PatrolTestRule: fix lint warning @DelicateCoroutinesApi

--- packages/patrol/android/src/main/kotlin/pl/leancode/patrol/PatrolTestRule.kt
+++ packages/patrol/android/src/main/kotlin/pl/leancode/patrol/PatrolTestRule.kt
@@ -3,12 +3,14 @@ package pl.leancode.patrol
 import android.app.Activity
 import android.content.Intent
 import androidx.test.rule.ActivityTestRule
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class PatrolTestRule<T : Activity>(activityClass: Class<T>) :
     ActivityTestRule<T>(activityClass, true, false) {
 
+    @OptIn(DelicateCoroutinesApi::class)
     override fun launchActivity(startIntent: Intent?): T {
         Logger.i("Starting server loop...")
 
@@ -18,6 +20,7 @@ class PatrolTestRule<T : Activity>(activityClass: Class<T>) :
         } catch (e: Exception) {
             Logger.e("Failure while running the server: ", e)
         }
+
 
         GlobalScope.launch {
             patrolServer.blockUntilShutdown()
