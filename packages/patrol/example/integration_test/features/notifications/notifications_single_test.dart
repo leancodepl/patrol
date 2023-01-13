30a261bffcacc5bf75431aee5ca08dc5697c2a31
Bartek Pacia <barpac02@gmail.com>
2023-01-13 16:48:54 +0100

    fix notifications tests on iOS (#768)

--- packages/patrol/example/integration_test/features/notifications/notifications_single_test.dart
+++ packages/patrol/example/integration_test/features/notifications/notifications_single_test.dart
@@ -6,7 +6,7 @@ import '../../common.dart';
 
 void main() {
   patrol(
-    'sends 1 notification, verifies that it is visible and taps on it',
+    'sends a notification, verifies that it is visible and taps on it by index',
     ($) async {
       await $.pumpWidgetAndSettle(ExampleApp());
 
@@ -29,6 +29,35 @@ void main() {
 
       expect(notifications.length, equals(1));
       await $.native.tapOnNotificationByIndex(0);
+    },
+  );
+
+  patrol(
+    'sends a notification, verifies that it is visible and taps on it by text',
+    ($) async {
+      await $.pumpWidgetAndSettle(ExampleApp());
+
+      await $('Open notifications screen').tap();
+      await $(RegExp('someone liked')).tap();
+
+      if (await $.native.isPermissionDialogVisible()) {
+        print('Dialog is visible');
+        await $.native.grantPermissionWhenInUse();
+      }
+
+      if (Platform.isIOS) {
+        await $.native.closeHeadsUpNotification();
+      }
+
+      await $.native.openNotifications();
+      final notifications = await $.native.getNotifications();
+      print('Found ${notifications.length} notifications');
+      notifications.forEach(print);
+
+      expect(notifications.length, equals(1));
+      await $.native.tapOnNotificationBySelector(
+        Selector(textContains: 'Tap to see who'),
+      );
     },
   );
 }
