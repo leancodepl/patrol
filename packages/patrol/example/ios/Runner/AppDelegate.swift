8ae15987d1dfd76389155206ebafabcb58c0d319
Bartek Pacia <bartek.pacia@leancode.pl>
2023-01-18 23:38:39 +0100

    setup permissions for notifications, location, and camera

--- packages/patrol/example/ios/Runner/AppDelegate.swift
+++ packages/patrol/example/ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
