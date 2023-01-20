0bed626fe633e7cd6a17ed41149dada202c32824
Bartek Pacia <barpac02@gmail.com>
2023-01-20 16:40:25 +0100

    Fix `patrol test` on Windows (#805)
    * AndroidAppOptions.toGradleInvocation(): depend on Platform
    
    * fix `patrol test` not working on Windows

--- packages/patrol_cli/test/features/test/android_test_backend_test.dart
+++ packages/patrol_cli/test/features/test/android_test_backend_test.dart
@@ -1,49 +1,96 @@
 import 'package:patrol_cli/src/features/test/android_test_backend.dart';
+import 'package:platform/platform.dart';
 import 'package:test/test.dart';
+
+final _windows = FakePlatform(operatingSystem: 'windows');
+final _macos = FakePlatform(operatingSystem: 'macos');
 
 void main() {
   group('AndroidAppOptions', () {
     late AndroidAppOptions options;
 
-    test('correctly encodes default invocation', () {
-      options = AndroidAppOptions(
-        target: '/home/john/flutterapp/integration_test/app_test.dart',
-        flavor: null,
-        dartDefines: {},
-      );
+    group('correctly encodes default invocation', () {
+      test('on Windows', () {
+        options = AndroidAppOptions(
+          target: r'C:\Users\john\app\integration_test\app_test.dart',
+          flavor: null,
+          dartDefines: {},
+        );
 
-      final invocation = options.toGradleInvocation();
-      expect(
-        invocation,
-        equals([
-          './gradlew',
-          ':app:connectedDebugAndroidTest',
-          '-Ptarget=/home/john/flutterapp/integration_test/app_test.dart',
-        ]),
-      );
+        final invocation = options.toGradleInvocation(_windows);
+        expect(
+          invocation,
+          equals([
+            'gradlew.bat',
+            ':app:connectedDebugAndroidTest',
+            r'-Ptarget=C:\Users\john\app\integration_test\app_test.dart',
+          ]),
+        );
+      });
+
+      test('on macOS', () {
+        options = AndroidAppOptions(
+          target: '/Users/john/app/integration_test/app_test.dart',
+          flavor: null,
+          dartDefines: {},
+        );
+
+        final invocation = options.toGradleInvocation(_macos);
+        expect(
+          invocation,
+          equals([
+            './gradlew',
+            ':app:connectedDebugAndroidTest',
+            '-Ptarget=/Users/john/app/integration_test/app_test.dart',
+          ]),
+        );
+      });
     });
 
-    test('correctly encodes customized invocation', () {
-      options = AndroidAppOptions(
-        target: '/home/john/flutterapp/integration_test/app_test.dart',
-        flavor: 'dev',
-        dartDefines: {
-          'EMAIL': 'user@example.com',
-          'PASSWORD': 'ny4ncat',
-          'foo': 'bar',
-        },
-      );
+    group('correctly encodes customized invocation', () {
+      const dartDefines = {
+        'EMAIL': 'user@example.com',
+        'PASSWORD': 'ny4ncat',
+        'foo': 'bar',
+      };
 
-      final invocation = options.toGradleInvocation();
-      expect(
-        invocation,
-        equals([
-          './gradlew',
-          ':app:connectedDevDebugAndroidTest',
-          '-Ptarget=/home/john/flutterapp/integration_test/app_test.dart',
-          '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbSxQQVNTV09SRD1ueTRuY2F0LGZvbz1iYXI='
-        ]),
-      );
+      test('on Windows', () {
+        options = AndroidAppOptions(
+          target: r'C:\Users\john\app\integration_test\app_test.dart',
+          flavor: 'dev',
+          dartDefines: dartDefines,
+        );
+
+        final invocation = options.toGradleInvocation(_windows);
+        expect(
+          invocation,
+          equals([
+            'gradlew.bat',
+            ':app:connectedDevDebugAndroidTest',
+            r'-Ptarget=C:\Users\john\app\integration_test\app_test.dart',
+            '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbSxQQVNTV09SRD1ueTRuY2F0LGZvbz1iYXI='
+          ]),
+        );
+      });
+
+      test('on macOS', () {
+        options = AndroidAppOptions(
+          target: '/Users/john/app/integration_test/app_test.dart',
+          flavor: 'dev',
+          dartDefines: dartDefines,
+        );
+
+        final invocation = options.toGradleInvocation(_macos);
+        expect(
+          invocation,
+          equals([
+            './gradlew',
+            ':app:connectedDevDebugAndroidTest',
+            '-Ptarget=/Users/john/app/integration_test/app_test.dart',
+            '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbSxQQVNTV09SRD1ueTRuY2F0LGZvbz1iYXI='
+          ]),
+        );
+      });
     });
   });
 }
