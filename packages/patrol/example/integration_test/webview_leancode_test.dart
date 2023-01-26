aaaa41f6e713055a8deec290e9d70b199923793d
Bartek Pacia <bartek.pacia@leancode.pl>
2023-01-26 18:48:12 +0100

    webview_leancode_test: fixing

--- packages/patrol/example/integration_test/webview_leancode_test.dart
+++ packages/patrol/example/integration_test/webview_leancode_test.dart
@@ -11,7 +11,7 @@ void testWebViewA() {
     await $('Open webview screen A').scrollTo().tap();
 
     await $.native.tap(Selector(text: 'Accept cookies'));
-    await $.native.tap(Selector(text: 'Select items'));
+    await $.native.tap(Selector(text: 'What do you do in IT?', instance: 1));
     await $.native.tap(Selector(text: 'Developer'));
     await $.native.tap(Selector(text: '1 item selected'));
     await $.native.enterTextByIndex('test@leancode.pl', index: 0);
