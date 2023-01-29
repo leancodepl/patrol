48617f842631db3945f78d5d188c4a286518f79d
Bartek Pacia <bartek.pacia@leancode.pl>
2023-01-29 23:41:56 +0100

    iOS and Android: rename classes to have matching names

--- packages/patrol/android/src/main/kotlin/pl/leancode/patrol/Automator.kt
+++ packages/patrol/android/src/main/kotlin/pl/leancode/patrol/Automator.kt
@@ -47,7 +47,7 @@ private fun fromUiObject2(obj: UiObject2): Contracts.NativeView {
     }
 }
 
-class PatrolAutomator private constructor() {
+class Automator private constructor() {
     private var timeoutMillis: Long = 10_000
 
     fun configure(waitForSelectorTimeout: Long) {
@@ -449,6 +449,6 @@ class PatrolAutomator private constructor() {
     }
 
     companion object {
-        val instance = PatrolAutomator()
+        val instance = Automator()
     }
 }
