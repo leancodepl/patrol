17575a567039b6eee13dcfd817969e33fa85409c
Bartek Pacia <bartek.pacia@leancode.pl>
2023-01-31 10:26:21 +0100

    revert changes in AppDelegate

--- packages/patrol/example/ios/Runner/AppDelegate.swift
+++ packages/patrol/example/ios/Runner/AppDelegate.swift
@@ -9,9 +9,6 @@ import Flutter
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-    
-    let testName = ProcessInfo.processInfo.environment["PATROL_TEST_NAME"]!
-    NSLog("HELLO! Patrol testName: %@", testName)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
