40e08f2093e22d0ec7fc3f7b8bd8fb5d38ce1dd1
Bartek Pacia <bartek.pacia@leancode.pl>
2023-02-16 14:56:57 +0100

    make example test fill text fields

--- packages/patrol/example/integration_test/webview_leancode_test.dart
+++ packages/patrol/example/integration_test/webview_leancode_test.dart
@@ -11,7 +11,14 @@ void testWebViewA() {
     await $('Open webview screen A').scrollTo().tap();
 
     await $.native.enterTextByIndex('test@hey.com', index: 0);
-    await $.native.enterTextByIndex('some pass', index: 0);
+    await $.native.enterTextByIndex('some pass', index: 1);
+    await $.native.tap(Selector(text: 'Done'));
+    await $.native.tap(
+      Selector(
+        text: 'Log in',
+        instance: 1,
+      ),
+    );
     // await $.native.enterText(
     //   Selector(text: 'user_email'), // "you@example.com", "Email", etc..
     //   text: 'bartek@awesome.com',
