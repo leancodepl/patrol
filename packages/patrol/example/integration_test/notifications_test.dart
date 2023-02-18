67058d1c9676b4100c1b479e7e4cd66e4c0dba80
Bartek Pacia <bartek.pacia@leancode.pl>
2023-02-18 15:07:43 +0100

    clean up notifications_test.dart

--- packages/patrol/example/integration_test/notifications_test.dart
+++ packages/patrol/example/integration_test/notifications_test.dart
@@ -7,28 +7,22 @@ void main() {
     'taps on notification',
     ($) async {
       await $.pumpWidgetAndSettle(ExampleApp());
-
       await $('Open notifications screen').tap();
 
       if (await $.native.isPermissionDialogVisible()) {
-        print('Dialog is visible');
         await $.native.grantPermissionWhenInUse();
       }
 
       await $(RegExp('someone liked')).tap();
-
       if (Platform.isIOS) {
         await $.native.closeHeadsUpNotification();
       }
 
       await $.native.openNotifications();
-      final notifications = await $.native.getNotifications();
-      print('Found ${notifications.length} notifications');
-      notifications.forEach(print);
-
       await $.native.tapOnNotificationBySelector(
         Selector(textContains: 'Tap to see who'),
       );
+
       await $('Tapped notification with ID: 1').waitUntilVisible();
     },
   );
