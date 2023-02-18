f9f883bcfeb0434e8726c9ecc7e1a6a8f5e1b74b
Bartek Pacia <bartek.pacia@leancode.pl>
2023-02-19 00:12:53 +0100

    PatrolBinding: clean up remainings (#970)
    * PatrolBinding: remove no longer functional members

--- packages/patrol/lib/src/binding.dart
+++ packages/patrol/lib/src/binding.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-import 'dart:developer';
-import 'dart:io' as io;
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -9,24 +6,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/common.dart';
 import 'package:integration_test/integration_test.dart';
-
-class _Response {
-  factory _Response.fromJson(Map<String, dynamic> json) {
-    return _Response._(
-      int.parse(json['request_id'] as String),
-      json['status'] == 'true',
-      json['error'] as String?,
-      json,
-    );
-  }
-
-  const _Response._(this.id, this.ok, this.error, this.raw);
-
-  final int id;
-  final bool ok;
-  final String? error;
-  final Map<String, dynamic> raw;
-}
 
 // ignore: avoid_print
 void _defaultPrintLogger(String message) {
@@ -73,7 +52,8 @@ class PatrolBinding extends IntegrationTestWidgetsFlutterBinding {
           return;
         }
 
-        debugPrint('Sending Dart test results to the native side');
+        // TODO: Migrate communication to gRPC
+        _logger('Sending Dart test results to the native side');
         await patrolChannel.invokeMethod<void>(
           'allTestsFinished',
           <String, dynamic>{
@@ -108,10 +88,6 @@ Thrown by PatrolBinding.
 
   final _logger = _defaultPrintLogger;
 
-  int _latestEventId = 0;
-
-  final _controller = StreamController<_Response>.broadcast();
-
   // TODO: Remove once https://github.com/flutter/flutter/pull/108430 is
   // available on the stable channel
   @override
@@ -121,78 +97,6 @@ Thrown by PatrolBinding.
   void initInstances() {
     super.initInstances();
     _instance = this;
-
-    if (!extensionStreamHasListener) {
-      _logger("Extension stream has no listeners, so host features won't work");
-    }
-  }
-
-  @override
-  void initServiceExtensions() {
-    super.initServiceExtensions();
-
-    if (!kReleaseMode) {
-      registerServiceExtension(
-        name: 'patrol',
-        callback: (args) async {
-          _controller.add(_Response.fromJson(args));
-          return <String, String>{};
-        },
-      );
-      _logger('registered service extension ext.flutter.patrol');
-    }
-  }
-
-  /// Takes a screenshot using the `flutter screenshot` command.
-  ///
-  /// The screenshot is placed in [path], named [name], and has .png extension.
-  Future<void> takeFlutterScreenshot({
-    required String name,
-    required String path,
-  }) async {
-    final eventId = ++_latestEventId;
-
-    postEvent('patrol', <String, dynamic>{
-      'method': 'take_screenshot',
-      'request_id': eventId,
-      'args': {'name': name, 'path': path}
-    });
-
-    final resp = await _controller.stream.firstWhere((r) => r.id == eventId);
-    if (!resp.ok) {
-      throw StateError('event with request_id $eventId failed: ${resp.error}');
-    }
-  }
-
-  /// Runs a process using [io.Process.run].
-  Future<io.ProcessResult> runProcess({
-    required String executable,
-    required List<String> arguments,
-    required bool runInShell,
-  }) async {
-    final eventId = ++_latestEventId;
-
-    postEvent('patrol', <String, dynamic>{
-      'method': 'run_process',
-      'request_id': eventId,
-      'args': {
-        'executable': executable,
-        'arguments': arguments,
-        'runInShell': runInShell,
-      },
-    });
-
-    final resp = await _controller.stream.firstWhere((r) => r.id == eventId);
-    if (!resp.ok) {
-      throw StateError('event with request_id $eventId failed: ${resp.error}');
-    }
-
-    return io.ProcessResult(
-      int.parse(resp.raw['pid'] as String),
-      int.parse(resp.raw['exitCode'] as String),
-      resp.raw['stdout'], // String
-      resp.raw['stderr'], // String
-    );
   }
 
   /// The singleton instance of this object.
