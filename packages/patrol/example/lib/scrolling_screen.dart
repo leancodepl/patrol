1ce9be23f09172e1bcb5deffdbb6f55c05ce1b45
Bartek Pacia <bartek.pacia@leancode.pl>
2023-03-02 22:14:34 +0100

    example app: make AppBar in ScrollingScreen togglable

--- packages/patrol/example/lib/scrolling_screen.dart
+++ packages/patrol/example/lib/scrolling_screen.dart
@@ -1,15 +1,24 @@
 import 'package:example/keys.dart';
 import 'package:flutter/material.dart';
 
-class ScrollingScreen extends StatelessWidget {
+class ScrollingScreen extends StatefulWidget {
   const ScrollingScreen({super.key});
+
+  @override
+  State<ScrollingScreen> createState() => _ScrollingScreenState();
+}
+
+class _ScrollingScreenState extends State<ScrollingScreen> {
+  bool showAppBar = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Scrolling'),
-      ),
+      appBar: showAppBar
+          ? AppBar(
+              title: const Text('Scrolling'),
+            )
+          : null,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -19,6 +28,11 @@ class ScrollingScreen extends StatelessWidget {
               'Some text at the top',
               key: K.topText,
               textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () => setState(() => showAppBar = !showAppBar),
+              child: Text('toggle app bar'),
             ),
             SizedBox(height: MediaQuery.of(context).size.height),
             const Text('Some text in the middle'),
