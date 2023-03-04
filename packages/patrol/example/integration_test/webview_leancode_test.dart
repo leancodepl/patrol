0b9b20e1142ba5ee8d9153291136e76e7127fd36
Bartek Pacia <bartek.pacia@leancode.pl>
2023-03-04 19:10:20 +0100

    find bug in webview_leancode_test.dart

--- packages/patrol/example/integration_test/webview_leancode_test.dart
+++ packages/patrol/example/integration_test/webview_leancode_test.dart
@@ -3,13 +3,18 @@ import 'common.dart';
 void main() async {
   patrol('interacts with the LeanCode website in a webview', ($) async {
     await createApp($);
-
     await $('Open webview (LeanCode)').scrollTo().tap();
-
     await $.native.tap(Selector(text: 'Accept cookies'));
+
+    // open dropdown
     await $.native.tap(Selector(text: 'What do you do in IT?', instance: 1));
+
+    // select option in dropdown
     await $.native.tap(Selector(text: 'Developer'));
-    await $.native.tap(Selector(text: '1 item selected'));
+
+    // close dropdown
+    await $.native.tap(Selector(text: 'What do you do in IT?', instance: 1));
+
     await $.native.enterTextByIndex('test@leancode.pl', index: 0);
   });
 }
