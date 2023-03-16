9598c3b264c51937a9f33c6e29366eef22244c9f
Bartek Pacia <bartek.pacia@leancode.pl>
2023-03-16 10:42:01 +0100

    add BuildMode tests for release mode

--- packages/patrol_cli/test/ios/ios_test_backend_test.dart
+++ packages/patrol_cli/test/ios/ios_test_backend_test.dart
@@ -4,20 +4,36 @@ import 'package:test/test.dart';
 void main() {
   group('IOSTestBackend', () {
     group('BuildMode', () {
-      test('infers build options when flavor is null', () {
-        String? flavor;
+      test('infers build options in debug mode when flavor is null', () {
         const buildMode = BuildMode.debug;
+        String? flavor;
 
         expect(buildMode.createConfiguration(flavor), 'Debug');
         expect(buildMode.createScheme(flavor), 'Runner');
       });
 
-      test('infers build options when flavor is not null', () {
-        const flavor = 'dev';
+      test('infers build options in debug mode when flavor is not null', () {
         const buildMode = BuildMode.debug;
+        const flavor = 'dev';
 
         expect(buildMode.createConfiguration(flavor), 'Debug-dev');
         expect(buildMode.createScheme(flavor), 'dev');
+      });
+
+      test('infers build options in release mode when flavor is null', () {
+        const buildMode = BuildMode.release;
+        String? flavor;
+
+        expect(buildMode.createConfiguration(flavor), 'Release');
+        expect(buildMode.createScheme(flavor), 'Runner');
+      });
+
+      test('infers build options in release mode when flavor is not null', () {
+        const buildMode = BuildMode.release;
+        const flavor = 'prod';
+
+        expect(buildMode.createConfiguration(flavor), 'Release-prod');
+        expect(buildMode.createScheme(flavor), 'prod');
       });
     });
   });
