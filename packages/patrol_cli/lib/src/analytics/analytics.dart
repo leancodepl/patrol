0f209fd920808f5088b5ef673c19c4d2c1587d19
Bartek Pacia <bartek.pacia@leancode.pl>
2023-03-30 22:12:23 +0200

    start over with `unified_analytics

--- packages/patrol_cli/lib/src/analytics/analytics.dart
+++ packages/patrol_cli/lib/src/analytics/analytics.dart
@@ -1,9 +1,22 @@
+// ignore: implementation_imports
+import 'package:unified_analytics/src/ga_client.dart';
+
 class Analytics {
   Analytics({
     required this.measurementId,
     required this.appName,
-  });
+  }) : _client = GAClient(
+          measurementId: measurementId,
+          apiSecret: 'apiSecret',
+        );
+
+  // TODO
 
   final String measurementId;
   final String appName;
+  final GAClient _client;
+
+  Future<void> sendEvent() {
+    _client.sendData(body);
+  }
 }
