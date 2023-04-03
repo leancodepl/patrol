4bd4a9d9284ea9d2507021a3776a6c4c814ce52c
Bartek Pacia <bartek.pacia@leancode.pl>
2023-04-04 00:01:51 +0200

    make `patrol update` also update `package:patrol` in pubspec

--- packages/patrol_cli/lib/src/commands/update.dart
+++ packages/patrol_cli/lib/src/commands/update.dart
@@ -2,25 +2,32 @@ import 'dart:async';
 
 import 'package:patrol_cli/src/analytics/analytics.dart';
 import 'package:patrol_cli/src/base/constants.dart';
+import 'package:patrol_cli/src/base/exceptions.dart';
 import 'package:patrol_cli/src/base/logger.dart';
+import 'package:patrol_cli/src/base/process.dart';
 import 'package:patrol_cli/src/runner/patrol_command.dart';
+import 'package:process/process.dart';
 import 'package:pub_updater/pub_updater.dart';
 
 class UpdateCommand extends PatrolCommand {
   UpdateCommand({
     required PubUpdater pubUpdater,
     required Analytics analytics,
+    required ProcessManager processManager,
     required Logger logger,
   })  : _pubUpdater = pubUpdater,
+        _processManager = processManager,
         _analytics = analytics,
         _logger = logger;
 
   final PubUpdater _pubUpdater;
+  final ProcessManager _processManager;
 
   final Analytics _analytics;
   final Logger _logger;
 
-  static const _pkg = 'patrol_cli';
+  static const _cli = 'patrol_cli';
+  static const _pkg = 'patrol';
 
   @override
   String get name => 'update';
@@ -32,40 +39,64 @@ class UpdateCommand extends PatrolCommand {
   Future<int> run() async {
     unawaited(_analytics.sendCommand(name));
 
-    Progress progress;
+    await _updatePatrolCliPackage();
+    await _updatePatrolPackage();
 
+    return 0;
+  }
+
+  Future<void> _updatePatrolCliPackage() async {
+    Progress progress;
+    progress = _logger.progress('Checking if newer $_cli version is available');
+
+    final String newVersion;
+    try {
+      newVersion = await _pubUpdater.getLatestVersion(_cli);
+    } catch (err) {
+      throwToolExit('Failed to check if newer $_cli version is available');
+    }
+
+    final isUpToDate = version == newVersion;
+    if (isUpToDate) {
+      progress.complete("You're on the latest $_cli version ($version)");
+      return;
+    }
+
+    progress.update('Updating $_cli to version $newVersion');
+    try {
+      await _pubUpdater.update(packageName: _cli);
+    } catch (err, st) {
+      progress.fail('Failed to update $_cli to version $newVersion');
+      _logger
+        ..err('$err')
+        ..err('$st');
+      return;
+    }
+
+    progress.complete('Updated $_cli to version $newVersion');
+  }
+
+  Future<void> _updatePatrolPackage() async {
+    Progress progress;
     progress = _logger.progress('Checking if newer $_pkg version is available');
 
     final String newVersion;
     try {
       newVersion = await _pubUpdater.getLatestVersion(_pkg);
-    } catch (err, st) {
-      progress.fail('Failed to check if newer $_pkg version is available');
-      _logger
-        ..err('$err')
-        ..err('$st');
-      return 1;
+    } catch (err) {
+      throwToolExit('Failed to check if newer $_pkg version is available');
     }
 
-    final isUpToDate = version == newVersion;
-    if (isUpToDate) {
-      progress.complete("You're on the latest $_pkg version ($version)");
-      return 0;
+    progress = _logger.progress('Updating patrol package');
+    final result = await _processManager.run(
+      ['flutter', '--no-version-check', 'pub', 'upgrade', 'patrol'],
+    );
+    if (result.exitCode != 0) {
+      throwToolExit('Failed to update patrol package');
+    } else if (result.stdOut.contains('No dependencies changed')) {
+      progress.complete("You're on the latest patrol version");
+    } else {
+      progress.complete('Updated $_pkg to version $newVersion');
     }
-
-    progress.complete('New $_pkg version is available ($newVersion)');
-    progress = _logger.progress('Updating $_pkg to version $newVersion');
-    try {
-      await _pubUpdater.update(packageName: _pkg);
-    } catch (err, st) {
-      progress.fail('Failed to update $_pkg to version $newVersion');
-      _logger
-        ..err('$err')
-        ..err('$st');
-      return 1;
-    }
-    progress.complete('Updated $_pkg to version $newVersion');
-
-    return 0;
   }
 }
