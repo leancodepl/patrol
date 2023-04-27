377c08022d3368813b855ef32bf004a2ee612744
fylyppo <ftuzimek@gmail.com>
2023-04-27 09:12:30 +0200

    Test woth more sleeping before kill

--- .github/scripts/run-emulator-and-tests.sh
+++ .github/scripts/run-emulator-and-tests.sh
@@ -10,7 +10,7 @@ while [ "`adb shell getprop sys.boot_completed | tr -d '\r' `" != "1" ] ; do sle
 # if we screenrecord too quickly, we get: "Unable to open '/sdcard/patrol.mp4': Operation not permitted"
 # sleep 60
 
-sleep 20
+sleep 60
 adb emu kill
 sleep 60
 
