238b67efd21427ae0a5be458c868b1bb674bcc81
fylyppo <ftuzimek@gmail.com>
2023-06-23 09:32:32 +0200

    add path to dependencies

--- packages/patrol/example/patrol_develop_test.dart
+++ packages/patrol/example/patrol_develop_test.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:path/path.dart' show join;
+import 'package:path/path.dart';
 
 void main() async {
   var isFirstTestPassed = false;
