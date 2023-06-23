2724264fdfdd9ec41f07573d84c5ea4c4b2ef2b9
fylyppo <ftuzimek@gmail.com>
2023-06-23 13:19:28 +0200

    revert

--- packages/patrol/example/integration_test/example_test.dart
+++ packages/patrol/example/integration_test/example_test.dart
@@ -4,7 +4,7 @@ import 'common.dart';
 
 void main() {
   patrol(
-    'This test is used to `test patrol develop`',
+    'counter state is the same after going to Home and switching apps',
     ($) async {
       await createApp($);
 
@@ -21,7 +21,7 @@ void main() {
       await $(FloatingActionButton).tap();
 
       expect($(#counterText).text, '2');
-      expect($('Hello, fail here!'), findsOneWidget);
+      expect($('Hello, Flutter!'), findsOneWidget);
     },
   );
 }
