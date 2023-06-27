4eaee2cd685298d075e853d899f2176e05607d26
Bartek Pacia <bartek.pacia@leancode.pl>
2023-06-27 19:10:33 +0200

    try workaround with webview warmup (pre-enter) - does not work

--- packages/patrol/example/integration_test/webview_hackernews_test.dart
+++ packages/patrol/example/integration_test/webview_hackernews_test.dart
@@ -17,6 +17,9 @@ void main() {
     }
     print('Found ${views.length} views');
 
+    await $.native.pressBack();
+    await $('Open webview (Hacker News)').scrollTo().tap();
+
     await $.native.tap(Selector(text: 'login'));
     await $.native.enterTextByIndex('test@leancode.pl', index: 0);
     await $.native.enterTextByIndex('ny4ncat', index: 1);
