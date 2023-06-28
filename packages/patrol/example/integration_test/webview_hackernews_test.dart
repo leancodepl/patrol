256b2cb2b82e58836fdadc2b73e43f4323073394
Bartek Pacia <bartek.pacia@leancode.pl>
2023-06-28 22:23:59 +0200

    remove `$.native.getNativeViews()`

--- packages/patrol/example/integration_test/webview_hackernews_test.dart
+++ packages/patrol/example/integration_test/webview_hackernews_test.dart
@@ -6,17 +6,6 @@ void main() {
 
     await $('Open webview (Hacker News)').scrollTo().tap();
 
-    final views = await $.native.getNativeViews(Selector(text: 'login'));
-    for (final view in views) {
-      print(
-        'Found view ${view.className}: text=${view.text}, contentDescription=${view.contentDescription}',
-      );
-    }
-    print('Found ${views.length} views');
-
-    await $.native.pressBack();
-    await $('Open webview (Hacker News)').scrollTo().tap();
-
     await $.native.tap(Selector(text: 'login'));
     await $.native.enterTextByIndex('test@leancode.pl', index: 0);
     await $.native.enterTextByIndex('ny4ncat', index: 1);
