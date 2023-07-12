5188b60a71eb3a05ad4bcca0952407c7922083c6
Bartek Pacia <bartek.pacia@leancode.pl>
2023-07-12 12:47:37 +0200

    simplify the workaround (remove starting a separate thread)

--- packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
+++ packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
@@ -15,23 +15,11 @@ import java.util.Enumeration;
 public class MainActivityTest {
     @Parameters(name = "{0}")
     public static Object[] testCases() {
-        PatrolGetter g = new PatrolGetter();
-        Thread t = new Thread(g);
-        t.start();
-        try {
-            t.join(60000);
-            if (g.result != null) {
-                t.interrupt();
-                return g.result;
-            } else {
-                Log.e("MATE", "Null result");
-                return new Object[0];
-            }
-        } catch (InterruptedException e) {
-            t.interrupt();
-            Log.e("MATE", "Failed getting");
-            return new Object[0];
-        }
+        listInterfaces();
+        CustomPatrolJUnitRunner instrumentation = (CustomPatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
+        instrumentation.setUp(MainActivity.class);
+        instrumentation.waitForPatrolAppService();
+        return instrumentation.listDartTests();
     }
 
     public MainActivityTest(String dartTestName) {
@@ -46,30 +34,17 @@ public class MainActivityTest {
         instrumentation.runDartTest(dartTestName);
     }
 
-    static class PatrolGetter implements Runnable {
-        public volatile Object[] result;
+    private static void listInterfaces() {
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface i = interfaces.nextElement();
+                Log.i("APPINET", "Interface: " + i.getDisplayName());
+                i.getInterfaceAddresses().forEach(a -> Log.i("APPINET", "    Address: " + a.toString() + "; " + a.getAddress().toString()));
 
-        @Override
-        public void run() {
-            listInterfaces();
-            CustomPatrolJUnitRunner instrumentation = (CustomPatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
-            instrumentation.setUp(MainActivity.class);
-            instrumentation.waitForPatrolAppService();
-            result = instrumentation.listDartTests();
-        }
-
-        private static void listInterfaces() {
-            try {
-                Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-                while (interfaces.hasMoreElements()) {
-                    NetworkInterface i = interfaces.nextElement();
-                    Log.i("APPINET", "Interface: " + i.getDisplayName());
-                    i.getInterfaceAddresses().forEach(a -> Log.i("APPINET", "    Address: " + a.toString() + "; " + a.getAddress().toString()));
-
-                }
-            } catch (SocketException e) {
-                Log.e("APPINET", "Cannot list interfaces: " + e.toString());
             }
+        } catch (SocketException e) {
+            Log.e("APPINET", "Cannot list interfaces: " + e.getMessage());
         }
     }
 }
