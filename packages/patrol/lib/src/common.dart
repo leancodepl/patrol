117ad9a41855420ea889bbf741448a0c1d0627a6
Bartek Pacia <bartek.pacia@leancode.pl>
2023-08-10 15:31:35 +0200

    fix missing import

--- packages/patrol/lib/src/common.dart
+++ packages/patrol/lib/src/common.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: invalid_use_of_internal_member, implementation_imports
 
 import 'dart:io' as io;
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:meta/meta.dart';
