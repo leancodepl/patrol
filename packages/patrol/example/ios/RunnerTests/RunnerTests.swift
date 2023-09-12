35823b3459b64221cafd5f030e99cbc00a19a2a0
Bartek Pacia <bartek.pacia@leancode.pl>
2023-09-12 14:41:32 +0200

    fix test names on iOS

--- packages/patrol/example/ios/RunnerTests/RunnerTests.swift
+++ packages/patrol/example/ios/RunnerTests/RunnerTests.swift
@@ -2,47 +2,87 @@ import XCTest
 import patrol
 
 final class RunnerTests: XCTestCase {
-  func testCreateMethodNameFromPatrolGeneratedGroup_ExampleWithDotAndUnderscore() {
+  func testCreateMethodNameFromPatrolGeneratedGroup_example1() {
+    let input = "example_test simple test"
+    let expectedOutput = "exampleTest___simpleTest"
+    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+    XCTAssertEqual(result, expectedOutput)
+  }
+
+  func testCreateMethodNameFromPatrolGeneratedGroup_example2() {
+    let input = "example.example_test group name test name"
+    let expectedOutput = "example_exampleTest___groupNameTestName"
+
+    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+    XCTAssertEqual(result, expectedOutput)
+  }
+
+  func testCreateMethodNameFromPatrolGeneratedGroup_example3() {
+    let input = "flows.login.sign_in logs into the app"
+    let expectedOutput = "flows_login_signIn___logsIntoTheApp"
+    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+    XCTAssertEqual(result, expectedOutput)
+  }
+
+  func testCreateMethodNameFromPatrolGeneratedGroup_example4() {
+    // Malformed edge case
+    let input = "single"
+    let expectedOutput = "single___"
+    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+    XCTAssertEqual(result, expectedOutput)
+  }
+
+  func testCreateMethodNameFromPatrolGeneratedGroup_example5() {
+    // Malformed edge case
+    let input = ""
+    let expectedOutput = "___"
+    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+    XCTAssertEqual(result, expectedOutput)
+  }
+
+  func testCreateMethodNameFromPatrolGeneratedGroup_example6() {
+    let input = "example_test at the beginning"
+    let expectedOutput = "exampleTest___atTheBeginning"  // FIXME: This is temporary
+    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+    XCTAssertEqual(result, expectedOutput)
+  }
+
+  func testConvertFirstPart_example1() {
+    let input = "example_test"
+    let expectedOutput = "exampleTest"
+
+    let result = PatrolUtils.convertFirstPart(input)
+    XCTAssertEqual(result, expectedOutput)
+  }
+
+  func testConvertFirstPart_example2() {
     let input = "example.example_test"
     let expectedOutput = "example_exampleTest"
 
-    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+    let result = PatrolUtils.convertFirstPart(input)
     XCTAssertEqual(result, expectedOutput)
   }
 
-  func testCreateMethodNameFromPatrolGeneratedGroup_ExampleWithUnderscoreOnly() {
-    let input = "example_test"
-    let expectedOutput = "exampleTest"
-    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
-    XCTAssertEqual(result, expectedOutput)
-  }
-
-  func testCreateMethodNameFromPatrolGeneratedGroup_ExampleWithMultipleDotsAndUnderscores() {
+  func testConvertFirstPart_example3() {
     let input = "flows.login.sign_in"
     let expectedOutput = "flows_login_signIn"
-    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+    let result = PatrolUtils.convertFirstPart(input)
     XCTAssertEqual(result, expectedOutput)
   }
 
-  func testCreateMethodNameFromPatrolGeneratedGroup_ExampleWithSingleComponent() {
-    let input = "single"
-    let expectedOutput = "single"
-    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
+  func testConvertSecondPart_example1() {
+    let input = "completes main flow"
+    let expectedOutput = "completesMainFlow"
+
+    let result = PatrolUtils.convertSecondPart(input)
     XCTAssertEqual(result, expectedOutput)
   }
 
-  func testCreateMethodNameFromPatrolGeneratedGroup_ExampleWithEmptyString() {
-    let input = ""
-    let expectedOutput = ""
-    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
-    XCTAssertEqual(result, expectedOutput)
-  }
+  func testConvertSecondPart_example2() {
+    let input = "first group secondTest completes_fast!!!"
+    let expectedOutput = "firstGroupSecondTestCompletes_fast"
 
-  func testCreateMethodNameFromPatrolGeneratedGroup_ExampleWithGroups() {
-    let input = "example_test at the beginning"
-    let expectedOutput = "exampleTestatthebeginning"  // FIXME: This is temporary
-    let result = PatrolUtils.createMethodName(fromPatrolGeneratedGroup: input)
-    NSLog(result)
+    let result = PatrolUtils.convertSecondPart(input)
     XCTAssertEqual(result, expectedOutput)
   }
 }
