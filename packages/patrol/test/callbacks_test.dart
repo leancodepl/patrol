e9b8e0490f13ba6f7a213f26f363cf61d4fade30
Bartek Pacia <bartek.pacia@leancode.pl>
2023-09-19 15:06:45 +0200

    start playing around

---
+++ packages/patrol/test/callbacks_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/src/extensions.dart';
+// ignore: depend_on_referenced_packages
+import 'package:test_api/src/backend/invoker.dart';
+
+const String requestedTest = 'groupA testA';
+
+String get currentTest => Invoker.current!.fullCurrentTestName();
+
+void main() {
+  patrolSetUp(() {
+    print('setting up before $currentTest');
+  });
+
+  patrolTearDown(() {
+    print('tearing down after $currentTest');
+  });
+
+  group('groupA', () {
+    test('testA', () {});
+    test('testB', () {});
+    test('testC', () {});
+  });
+}
+
+void patrolSetUp(dynamic Function() body) {
+  setUp(() {
+    final currentTest = Invoker.current!.fullCurrentTestName();
+
+    // TODO: Determine if requestedTest is inside this setUps scope?
+    // Hipothesis: package:test cares about this automatically!
+    if (currentTest == requestedTest) {
+      body();
+    }
+  });
+}
+
+void patrolTearDown(dynamic Function() body) {
+  tearDown(() {
+    final currentTest = Invoker.current!.fullCurrentTestName();
+
+    if (currentTest == requestedTest) {
+      body();
+    }
+  });
+}
