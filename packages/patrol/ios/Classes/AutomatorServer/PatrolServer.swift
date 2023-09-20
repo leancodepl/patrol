6680bfc5369e0ddefe068bdae097d36a7583f050
Damian Cudzik <cudzikdam@gmail.com>
2023-09-20 14:44:22 +0200

    Fix swift-format issues

--- packages/patrol/ios/Classes/AutomatorServer/PatrolServer.swift
+++ packages/patrol/ios/Classes/AutomatorServer/PatrolServer.swift
@@ -44,7 +44,7 @@ import Telegraph
     #endif
   }
 
-  @objc public func start() throws -> Void {
+  @objc public func start() throws {
     #if PATROL_ENABLED
       Logger.shared.i("Starting server...")
 
--- packages/patrol_gen/lib/src/generators/ios/ios_url_session_client_generator.dart
+++ packages/patrol_gen/lib/src/generators/ios/ios_url_session_client_generator.dart
@@ -31,8 +31,8 @@ class IOSURLSessionClientGenerator {
 
     const url = r'http://\(address):\(port)/\(requestName)';
 
-    const throwException =
-        r'throw PatrolError.internal("Invalid response: \(response) \(data)")';
+    const exceptionMessage =
+        r'Invalid response: \(String(describing: response)) \(String(describing: data))';
 
     return '''
 class ${service.name}Client {
@@ -48,7 +48,9 @@ class ${service.name}Client {
 
 $endpoints
 
-  private func performRequest<TResult: Codable>(requestName: String, body: Data? = nil) async throws -> TResult {
+  private func performRequest<TResult: Codable>(
+    requestName: String, body: Data? = nil, completion: @escaping (Result<TResult, Error>) -> Void
+  ) {
     let url = URL(string: "$url")!
 
     let urlconfig = URLSessionConfiguration.default
@@ -60,32 +62,58 @@ $endpoints
     request.httpBody = body
     request.timeoutInterval = timeout
 
-    let (data, response) = try await URLSession(configuration: urlconfig).data(for: request)
-    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-        $throwException
-    }
+    let session = URLSession(configuration: urlconfig)
 
-    return try JSONDecoder().decode(TResult.self, from: data)
+    session.dataTask(with: request) { data, response, error in
+      if (response as? HTTPURLResponse)?.statusCode == 200 {
+        do {
+          let object = try JSONDecoder().decode(TResult.self, from: data!)
+          completion(.success(object))
+        } catch let err {
+          completion(.failure(err))
+        }
+      } else {
+        let message =
+          "$exceptionMessage"
+        completion(.failure(PatrolError.internal(message)))
+      }
+    }.resume()
   }
 }
 ''';
   }
 
   String _createEndpoint(Endpoint endpoint) {
-    final parameterDef =
+    final requestDef =
         endpoint.request != null ? 'request: ${endpoint.request!.name}' : '';
-    final returnDef =
-        endpoint.response != null ? '-> ${endpoint.response!.name}' : '';
+
+    final completionDef = endpoint.response != null
+        ? 'completion: @escaping (Result<${endpoint.response!.name}, Error>) -> Void'
+        : 'completion: @escaping (Result<Void, Error>) -> Void';
+
+    final parameters = endpoint.request != null
+        ? '$requestDef, $completionDef'
+        : completionDef;
+
+    final arguments = [
+      'requestName: "${endpoint.name}"',
+      if (endpoint.request != null) 'body: body',
+      'completion: completion'
+    ].join(', ');
 
     final bodyCode = endpoint.request != null
-        ? '\n    let body = try JSONEncoder().encode(request)'
-        : '';
-
-    final bodyArgument = bodyCode.isNotEmpty ? ', body: body' : '';
+        ? '''
+    do {
+      let body = try JSONEncoder().encode(request)
+      performRequest($arguments)
+    } catch let err {
+      completion(.failure(err))
+    }'''
+        : '    performRequest($arguments)';
 
     return '''
-  func ${endpoint.name}($parameterDef) async throws $returnDef {$bodyCode
-    return try await performRequest(requestName: "${endpoint.name}"$bodyArgument)
+  func ${endpoint.name}($parameters) {
+$bodyCode
   }''';
   }
 }
