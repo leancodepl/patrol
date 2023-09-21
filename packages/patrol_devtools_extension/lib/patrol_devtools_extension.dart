1ff42c426b09aa9a704219addbb319bac7ad15cb
Damian Cudzik <cudzikdam@gmail.com>
2023-09-21 12:47:24 +0200

    Add simple extension and publish script

---
+++ packages/patrol_devtools_extension/lib/patrol_devtools_extension.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class PatrolDevToolsExtension extends StatelessWidget {
+  const PatrolDevToolsExtension({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
