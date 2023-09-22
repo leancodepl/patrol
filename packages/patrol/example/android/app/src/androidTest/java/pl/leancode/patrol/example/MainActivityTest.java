1e9fd9ea6dcd990da4aac0a6671993fac06648c9
Bartek Pacia <bartek.pacia@leancode.pl>
2023-09-22 14:51:04 +0200

    try to save data to instrumentation's storage

--- packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
+++ packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
@@ -1,11 +1,15 @@
 package pl.leancode.patrol.example;
 
+import android.util.Log;
 import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import pl.leancode.patrol.PatrolJUnitRunner;
+
+import java.io.File;
+import java.io.IOException;
 
 @RunWith(Parameterized.class)
 public class MainActivityTest {
@@ -23,9 +27,33 @@ public class MainActivityTest {
 
     private final String dartTestName;
 
+    private void log(String text) {
+        Log.d("MainActivity log", text);
+    }
+
     @Test
-    public void runDartTest() {
+    public void runDartTest() throws IOException {
         PatrolJUnitRunner instrumentation = (PatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
+
+// targetContext (app under test) data is cleared on each test run
+//        File cacheDir = instrumentation.getTargetContext().getCacheDir();
+//        File externalCacheDir = instrumentation.getTargetContext().getExternalCacheDir();
+
+        File cacheDir = instrumentation.getContext().getCacheDir();
+        boolean exists = cacheDir.exists();
+        boolean created = cacheDir.mkdirs();
+        log("cacheDir.mkdir(): exists=" + exists + ", created=" + created);
+
+        log("cacheDir: " + cacheDir);
+
+        File cacheDirFile = new File(cacheDir, "patrol.txt");
+        created = cacheDirFile.createNewFile();
+        log("cacheDirFile.createNewFile(): created=" + created);
+//
+//        File externalCacheDirFile = new File(externalCacheDir, "patrol.txt");
+//        created = externalCacheDirFile.createNewFile();
+//        log("externalCacheDirFile.createNewFile(): created=" + created);
+
         instrumentation.runDartTest(dartTestName);
     }
 }
