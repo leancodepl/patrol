a84464f9c74d0dae344183cd81b7ce70000fde6a
Bartek Pacia <bartek.pacia@leancode.pl>
2023-09-29 10:59:54 +0200

    write persistent file to `/sdcard/googletest/test_outputfiles`

--- packages/patrol/android/src/main/kotlin/pl/leancode/patrol/CustomPatrolJUnitRunner.kt
+++ packages/patrol/android/src/main/kotlin/pl/leancode/patrol/CustomPatrolJUnitRunner.kt
@@ -1,28 +1,28 @@
 package pl.leancode.patrol
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.OptIn
 import androidx.test.annotation.ExperimentalTestApi
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.services.storage.TestStorage
-import org.junit.runner.*
-import org.junit.runner.notification.*
-import java.io.FileNotFoundException
+import org.junit.runner.Description
+import org.junit.runner.notification.RunListener
 
 @OptIn(ExperimentalTestApi::class)
 class CustomPatrolJUnitRunner : PatrolJUnitRunner() {
     private var testStorage: TestStorage? = null
-    override fun onCreate(arguments: Bundle) {
-        super.onCreate(arguments)
-        testStorage = TestStorage()
-        try {
-            testStorage!!.openOutputFile("patrol_state/teardowns.txt")
-        } catch (e: FileNotFoundException) {
-            throw RuntimeException(e)
-        }
 
-        val listeners = listOf(
+    override fun onCreate(arguments: Bundle) {
+
+//        try {
+//            val outputStream = testStorage!!.openOutputFile("patrol_state/teardowns.txt")
+//        } catch (e: FileNotFoundException) {
+//            throw RuntimeException(e)
+//        }
+
+        val listeners = listOfNotNull(
             arguments.getCharSequence("listener"),
             PatrolStoragePermissionListener::class.java.name,
         ).joinToString(separator = ",")
@@ -30,11 +30,27 @@ class CustomPatrolJUnitRunner : PatrolJUnitRunner() {
         arguments.putCharSequence("listener", listeners)
         super.onCreate(arguments)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        testStorage = TestStorage()
+    }
 }
 
+@SuppressLint("UnsafeOptInUsageError")
 class PatrolStoragePermissionListener : RunListener() {
-    @Throws(Exception::class)
-    override fun testRunStarted(description: Description) {
+
+    val testStorage by lazy { TestStorage() }
+
+    override fun testStarted(description: Description) {
+        Logger.d("testStarted with description: $description")
+
+
+        val os = testStorage.openOutputFile("patrol.txt", true)
+        os.write("testStarted with description: $description\n".toByteArray())
+
+
         InstrumentationRegistry.getInstrumentation().uiAutomation.apply {
             val testServicesPackage = "androidx.test.services"
             when {
