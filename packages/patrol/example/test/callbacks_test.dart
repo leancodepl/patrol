6045fa7a211772b2b58b29e4bd5f7700ff53a73c
Bartek Pacia <bartek.pacia@leancode.pl>
2023-10-09 20:02:40 +0200

    remember setUpAll callbacks

--- packages/patrol/example/test/callbacks_test.dart
+++ packages/patrol/example/test/callbacks_test.dart
@@ -3,31 +3,76 @@ import 'package:patrol/src/extensions.dart';
 // ignore: depend_on_referenced_packages
 import 'package:test_api/src/backend/invoker.dart';
 
+// Input:
 const String requestedTest = 'testA';
+
+// Global state:
 
 String get currentTest => Invoker.current!.fullCurrentTestName();
 
-void main() {
-  group('alpha', () {
-    setUpAll(() {
-      final groupName = Invoker.current!.liveTest.groups.last.name;
-      final individualName = Invoker.current!.liveTest.individualName;
-      print(
-        'setUpAll: parentGroupName=$groupName, individualName=$individualName',
-      );
-    });
+/// A list holding IDs of all setUpAll callbacks.
+///
+/// This is basically the list of groupNames + an index appended.
+List<String> setUpAlls = [];
 
-    setUp(() {
-      final groupName = Invoker.current!.liveTest.groups.last.name;
-      final individualName = Invoker.current!.liveTest.individualName;
-      print(
-        'setUpAll: parentGroupName=$groupName, individualName=$individualName',
-      );
+/// Adds a setUpAll callback to the list of all setUpAll callbacks.
+///
+/// Returns the name under which this setUpAll callback was added.
+String addSetUpAll(String group) {
+  // Not optimal, but good enough for now.
+
+  // Go over all groups, checking if the group is already in the list.
+  var groupIndex = 0;
+  for (final setUpAll in setUpAlls) {
+    final parts = setUpAll.split(' ');
+    final groupName = parts.sublist(0, parts.length - 1).join(' ');
+
+    print('Checking if $groupName is in the list');
+
+    if (groupName == group) {
+      groupIndex++;
+    }
+  }
+
+  final name = '$group $groupIndex';
+
+  setUpAlls.add(name);
+  return name;
+}
+
+void printSetUpAlls() {
+  print('setUpAlls:');
+  for (final setUpAll in setUpAlls) {
+    print('  $setUpAll');
+  }
+}
+
+void main() {
+  group('A', () {
+    customSetUpAll(() async {});
+
+    customSetUpAll(() async {});
+
+    group('B', () {
+      customSetUpAll(() async {});
+      customSetUpAll(() async {});
+      customSetUpAll(() async {});
+      customSetUpAll(() async {});
+
+      group('C', () {
+        customSetUpAll(() async {});
+        customSetUpAll(() async {});
+      });
     });
 
     patrolTest('testA', _body);
     patrolTest('testB', _body);
     patrolTest('testC', _body);
+  });
+
+  tearDownAll(() {
+    print('Reporting status!');
+    printSetUpAlls();
   });
 }
 
@@ -41,5 +86,35 @@ void patrolTest(String name, Future<void> Function() body) {
       print('Requested test $currentTest, will execute it');
       await body();
     }
+  });
+}
+
+/// A modification of [setUpAll] that works with Patrol's native automation.
+///
+/// Its main purpose is to keep track of calls made to setUpAll.
+///
+/// groupA
+///  - setUpAll
+///  - setUpAll
+///  - groupB
+///   - setUpAll
+void customSetUpAll(Future<void> Function() body) {
+  setUpAll(() async {
+    final parentGroupsName = Invoker.current!.liveTest.groups.last.name;
+
+    final name = addSetUpAll(parentGroupsName);
+
+    // if (!isInTestDiscoveryPhase) {
+    //   return;
+    // }
+
+    // if (patrolAppService.wasSetUpAllExecuted()) {}
+
+    // final requestedToExecute = await PatrolBinding.instance.patrolAppService
+    //     .waitForExecutionRequest(currentSetUpAllIndex);
+
+    // if (requestedToExecute) {
+    //   await body();
+    // }
   });
 }
