636a476b3af64a5163be8580080c7cc0cf8876cd
Bartek Pacia <bartek.pacia@leancode.pl>
2023-10-17 11:22:52 +0200

    logs.dart: fix analyzer warnings

--- packages/patrol/lib/src/logs.dart
+++ packages/patrol/lib/src/logs.dart
@@ -1,8 +1,13 @@
 import 'package:meta/meta.dart';
 
-final runKey = Object().hashCode;
+final _runKey = Object().hashCode;
 
+/// Logs a message with a tag that's constant in the single app process
+/// instance.
+///
+/// Helps to differentiate same logs that happen in different app process runs.
 @internal
 void patrolDebug(String message) {
-  print('PATROL_DEBUG($runKey): $message');
+  // ignore: avoid_print
+  print('PATROL_DEBUG($_runKey): $message');
 }
