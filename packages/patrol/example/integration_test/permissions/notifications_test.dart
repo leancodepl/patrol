83184329adb524befb4771d256ecc60c6ba33a8d
Piotr Maszota <pmaszota98@gmail.com>
2023-11-17 13:33:04 +0100

    Use workaround for checkign permission page & remove additional workflow

--- packages/patrol/example/integration_test/permissions/notifications_test.dart
+++ packages/patrol/example/integration_test/permissions/notifications_test.dart
@@ -11,14 +11,17 @@ void main() {
         await $.native.grantPermissionWhenInUse();
       }
 
-      // Android 14+ requires additional permission to schedule notifications
-      try {
-        await $.native.tap(
-          Selector(text: 'Allow setting alarms and reminders'),
-        );
+      // Android 14+ requires additional permission to schedule notifications.
+      // Workaround for conditionally granting permission.
+      final android14PermissionSelector = Selector(
+        text: 'Allow setting alarms and reminders',
+      );
+      final android14PermissionScreen = await $.native.getNativeViews(
+        android14PermissionSelector,
+      );
+      if (android14PermissionScreen.isNotEmpty) {
+        await $.native.tap(android14PermissionSelector);
         await $.native.pressBack();
-      } on PatrolActionException catch (_) {
-        // ignore
       }
 
       await $('Show in a few seconds').tap();
