123855c3def842262cb885457e11da91df33fb09
Bartek Pacia <bartek.pacia@leancode.pl>
2023-12-19 01:36:57 +0100

    fix `patrolSetUp` and `patrolTearDown` not working in `patrol develop`
    add simple smoke test

--- dev/e2e_app/integration_test/example_test.dart
+++ dev/e2e_app/integration_test/example_test.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'common.dart';
 
 void main() {
+  patrolSetUp(() {
+    // Smoke test for https://github.com/leancodepl/patrol/issues/2021
+    expect(2 + 2, equals(4));
+  });
+
   patrol(
     'counter state is the same after going to Home and switching apps',
     ($) async {
