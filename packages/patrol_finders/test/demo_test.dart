8e98d14bacacb3f6ae522ed093f3e8177325d61d
Bartek Pacia <bartek.pacia@leancode.pl>
2023-12-20 13:22:21 +0100

    commit from stuff we did on workshop

--- packages/patrol_finders/test/demo_test.dart
+++ packages/patrol_finders/test/demo_test.dart
@@ -3,11 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol_finders/src/custom_finders/custom_finders.dart';
 
 void main() {
-  const app = MaterialApp(
+  final app = MaterialApp(
     home: Row(
       children: [
-        Icon(Icons.front_hand, key: ValueKey({'key': 'icon'})),
-        Text('Hello', key: Key('helloText')),
+        const Icon(Icons.front_hand, key: ValueKey({'key': 'icon'})),
+        const Text('Hello', key: Key('helloText')),
+        TextButton(
+          onPressed: () {
+            final up = Exception('This is an exception');
+            throw up; // haha
+          },
+          child: const Text('button'),
+        ),
       ],
     ),
   );
@@ -15,11 +22,19 @@ void main() {
   patrolWidgetTest('find by key', ($) async {
     await $.pumpWidget(app);
 
+    //await $.enterText($(#nonExistentTextField), 'username');
+    // exit(69);
+
     expect($(#helloText), findsOneWidget);
     expect($(const Symbol('helloText')), findsOneWidget);
     expect($(const Key('helloText')), findsOneWidget);
 
     expect($(const ValueKey({'key': 'icon'})), findsOneWidget);
     expect($(const ValueKey({'key': 'icon1'})), findsNothing);
+  });
+
+  patrolWidgetTest('bad app that throws exception', ($) async {
+    await $.pumpWidget(app);
+    await $('button').tap();
   });
 }
