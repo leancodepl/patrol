c2bfebf208e82e61d25666c84827c0e4dd253c89
fylyppo <ftuzimek@gmail.com>
2024-01-29 11:51:35 +0100

    Add rule to retry tests

--- packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
+++ packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
@@ -1,6 +1,9 @@
 package pl.leancode.patrol.example;
 
+import pl.leancode.patrol.example.RetryTestRule;
+
 import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -22,6 +25,9 @@ public class MainActivityTest {
     }
 
     private final String dartTestName;
+
+    @Rule
+    public RetryTestRule retryTestRule = new RetryTestRule(3);
 
     @Test
     public void runDartTest() {
