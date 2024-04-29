c09f01602c660ddb924767563160212873b3ad26
Akshatji800 <akshatji.tripathi@gmail.com>
2024-04-29 11:14:05 +0530

    Adds documentation for delayBetweenTapsMillis

--- packages/patrol/lib/src/native/native_automator2.dart
+++ packages/patrol/lib/src/native/native_automator2.dart
@@ -425,6 +425,15 @@ class NativeAutomator2 {
   /// [timeout] is not specified, it utilizes the
   /// [NativeAutomatorConfig.findTimeout] duration from the configuration.
   /// If the native view is not found, an exception is thrown.
+  ///
+  /// The [delayBetweenTapsMillis] parameter allows you to specify the duration
+  /// between consecutive taps in milliseconds. This can be useful in scenarios
+  /// where the target view requires a certain delay between taps to register
+  /// the action correctly, such as in cases of UI responsiveness or animations.
+  /// The default delay between taps is 300 milliseconds.
+  ///
+  /// Note: The [delayBetweenTapsMillis] parameter is currently respected only
+  /// for Android.
   Future<void> doubleTap(
     NativeSelector selector, {
     String? appId,
