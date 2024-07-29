1f9577ddf8ad29687a8ad2bfb308c4e46a890bb2
pdenert <pdenert09@gmail.com>
2024-07-29 13:16:49 +0200

    Revert changes in MainActivityTests

--- packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
+++ packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
@@ -1,41 +1,30 @@
 package pl.leancode.patrol.e2e_app;
 
-import static org.junit.Assume.*;
-
-import java.util.Collection;
-
 import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 import pl.leancode.patrol.PatrolJUnitRunner;
 
-
 @RunWith(Parameterized.class)
 public class MainActivityTest {
-    @Parameters 
-    public static Collection<Object[]> testCases() {
+    @Parameters(name = "{0}")
+    public static Object[] testCases() {
         PatrolJUnitRunner instrumentation = (PatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
         instrumentation.setUp(MainActivity.class);
         instrumentation.waitForPatrolAppService();
         return instrumentation.listDartTests();
     }
 
-    public MainActivityTest(String dartTestName, Boolean skip) {
+    public MainActivityTest(String dartTestCaseNamesList) {
         this.dartTestName = dartTestName;
-        this.skip = skip;
     }
 
     private String dartTestName;
 
-    private Boolean skip;
-
     @Test
     public void runDartTest() {
-        assumeFalse(skip);
-
         PatrolJUnitRunner instrumentation = (PatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
         instrumentation.runDartTest(dartTestName);
     }
