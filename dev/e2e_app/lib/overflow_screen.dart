ceb8b92cb921a99fc48b795bce31643d5400d654
karolinaoparczyk <karolinaoparczyk@gmail.com>
2024-10-08 15:49:03 +0200

    Rename test and comment exceptions on screen

--- dev/e2e_app/lib/overflow_screen.dart
+++ dev/e2e_app/lib/overflow_screen.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class OverflowScreen extends StatelessWidget {
   const OverflowScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Future<void> throwStackOverflowException() async {
-      await Future<void>.delayed(const Duration(milliseconds: 100));
-      throw StackOverflowError();
-    }
-
-    Future<void> throwPlatformException() async {
-      await Future<void>.delayed(const Duration(milliseconds: 150));
-      throw PlatformException(code: 'code');
-    }
-
-    throwStackOverflowException();
-    throwPlatformException();
+    // Uncomment to test `Multiple exceptions were thrown` issue
+    // Future<void> throwStackOverflowException() async {
+    //   await Future<void>.delayed(const Duration(milliseconds: 100));
+    //   throw StackOverflowError();
+    // }
+    //
+    // Future<void> throwPlatformException() async {
+    //   await Future<void>.delayed(const Duration(milliseconds: 150));
+    //   throw PlatformException(code: 'code');
+    // }
+    //
+    // throwStackOverflowException();
+    // throwPlatformException();
 
     return Scaffold(
       appBar: AppBar(
@@ -25,11 +25,13 @@ class OverflowScreen extends StatelessWidget {
       ),
       body: Row(
         children: const <Widget>[
-          SizedBox(
-            width: 500,
-            child: Text('This container is too wide for the row'),
-          ),
+          // Uncomment to test `Multiple exceptions were thrown` issue
+          // SizedBox(
+          //   width: 500,
+          //   child: Text('This container is too wide for the row'),
+          // ),
           Icon(Icons.star, size: 50),
+          Icon(Icons.abc, size: 50),
         ],
       ),
     );
