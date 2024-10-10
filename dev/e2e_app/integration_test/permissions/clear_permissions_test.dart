8da17cafb9288386f67cdfb7c1f00a74ffafa600
karolinaoparczyk <karolinaoparczyk@gmail.com>
2024-10-10 10:02:29 +0200

    Reset many permissions

--- dev/e2e_app/integration_test/permissions/clear_permissions_test.dart
+++ dev/e2e_app/integration_test/permissions/clear_permissions_test.dart
@@ -9,6 +9,7 @@ void main() {
     await $('Open permissions screen').scrollTo().tap();
 
     await _requestAndGrantCameraPermission($);
+    await _requestAndGrantMicrophonePermission($);
   });
   patrol('grants various permissions 2', ($) async {
     await createApp($);
@@ -16,6 +17,7 @@ void main() {
     await $('Open permissions screen').scrollTo().tap();
 
     await _requestAndGrantCameraPermission($);
+    await _requestAndGrantMicrophonePermission($);
   });
 }
 
@@ -24,6 +26,17 @@ Future<void> _requestAndGrantCameraPermission(PatrolIntegrationTester $) async {
   await $('Request camera permission').tap();
   if (await $.native.isPermissionDialogVisible(timeout: _timeout)) {
     await $.native.grantPermissionWhenInUse();
+    await $.pump();
+  }
+}
+
+Future<void> _requestAndGrantMicrophonePermission(
+  PatrolIntegrationTester $,
+) async {
+  expect($(#microphone).$(#statusText).text, 'Not granted');
+  await $('Request microphone permission').tap();
+  if (await $.native.isPermissionDialogVisible(timeout: _timeout)) {
+    await $.native.grantPermissionOnlyThisTime();
     await $.pump();
   }
 }
