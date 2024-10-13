d4838f755c1ea8cfc2a99d2d6b3dae887986ff0c
piotruela <piotr.maszota@leancode.co>
2024-10-13 13:05:09 +0200

    Prepare script for running tests in flutter test repo

---
+++ scripts/flutter_test_repo_test.sh
@@ -0,0 +1,16 @@
+# Script invoked by the Flutter customer tests repository.
+
+set -e
+
+# Array of relative directories
+dirs=("packages/patrol_finders" "packages/patrol" "packages/patrol_cli" "packages/patrol_devtools_extension")
+
+# Loop through each directory
+for dir in "${dirs[@]}"; do
+  cd "$dir"
+
+  flutter analyze --no-fatal-infos
+  flutter test
+
+  cd - > /dev/null # Return to the previous directory
+done
