627035b387f2ae03c7d25936ff2b2039801f8094
pdenert <pdenert09@gmail.com>
2024-11-22 11:10:35 +0100

    Reorganize ansi_codes

--- packages/patrol_log/lib/src/ansi_codes.dart
+++ packages/patrol_log/lib/src/ansi_codes.dart
@@ -8,17 +8,15 @@ class AnsiCodes {
   /// `[1m` - Set text to bold.
   static const String bold = '$escape[1m';
 
-  /// `[30m` - Set text color to gray.
-  static const String gray = '$escape[30m';
+  static String color(String color) => '$escape[${color}m';
 
-  /// `[38;5;87m` - Set text color to light blue. Used for native actions.
-  static const String lightBlue = '$escape[38;5;87m';
-
-  static String custom(String color) => '$escape[${color}m';
-
-  static String get green => custom('32');
-  static String get yellow => custom('33');
-  static String get blue => custom('34');
-  static String get magenta => custom('35');
-  static String get cyan => custom('36');
+  static String gray = color('30');
+  static String get red => color('31');
+  static String get green => color('32');
+  static String get yellow => color('33');
+  static String get blue => color('34');
+  static String get lightBlue => color('38;5;87');
+  static String get magenta => color('35');
+  static String get cyan => color('36');
+  static String get orange => color('38;5;208');
 }
