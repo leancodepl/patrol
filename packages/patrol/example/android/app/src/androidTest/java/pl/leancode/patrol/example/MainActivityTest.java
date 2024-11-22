f5e452fb077e9cb72200ff59c1fd017cb432e934
Gauravpandeyr <pandeyg021@gmail.com>
2024-11-22 15:27:06 +0530

    changes for lambdatest runner

--- packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
+++ packages/patrol/example/android/app/src/androidTest/java/pl/leancode/patrol/example/MainActivityTest.java
@@ -6,14 +6,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import pl.leancode.patrol.PatrolJUnitRunner;
-import pl.leancode.patrol.LambdatestPatrolJUnitRunner;
-
 
 @RunWith(Parameterized.class)
 public class MainActivityTest {
     @Parameters(name = "{0}")
     public static Object[] testCases() {
-        LambdatestPatrolJUnitRunner instrumentation = (LambdatestPatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
+        PatrolJUnitRunner instrumentation = (PatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
         instrumentation.setUp(MainActivity.class);
         instrumentation.waitForPatrolAppService();
         return instrumentation.listDartTests();
@@ -27,7 +25,7 @@ public class MainActivityTest {
 
     @Test
     public void runDartTest() {
-        LambdatestPatrolJUnitRunner instrumentation = (LambdatestPatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
+        PatrolJUnitRunner instrumentation = (PatrolJUnitRunner) InstrumentationRegistry.getInstrumentation();
         instrumentation.runDartTest(dartTestName);
     }
 }
