628823051085c01bb2c94b8787a6c98ace3460a7
piotruela <piotr.maszota@leancode.co>
2025-02-12 20:07:59 +0100

    Use ffi for sending ios port

--- packages/patrol/darwin/Classes/AutomatorServer/GlobalPort.swift
+++ packages/patrol/darwin/Classes/AutomatorServer/GlobalPort.swift
@@ -2,10 +2,7 @@ import Foundation
 
 var globalPort: Int32 = 0
 
-#if !PATROL_ENABLED
-@objc(IOSServerPortProvider123) public class IOSServerPortProvider : NSObject {
-    @objc public static func getGlobalPort() -> Int32 {
-        return globalPort
-    }
+@_cdecl("getGlobalPort")
+public func getGlobalPort() -> Int32 {
+  return globalPort
 }
-#endif
