305a1d55e0a44888fb5bde0ec7713f5fbb989207
piotruela <piotr.maszota@leancode.co>
2025-03-03 18:42:11 +0100

    Remove unused PatrolPlugin import in MainActivityTest

--- dev/e2e_app/android/app/src/androidTest/java/pl/leancode/patrol/e2e_app/MainActivityTest.java
+++ dev/e2e_app/android/app/src/androidTest/java/pl/leancode/patrol/e2e_app/MainActivityTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import pl.leancode.patrol.PatrolJUnitRunner;
+import pl.leancode.patrol.PatrolServer;
 
 @RunWith(Parameterized.class)
 public class MainActivityTest {
