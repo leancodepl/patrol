32031fe1056c1fcaa78422ba61dec89120bdb107
piotruela <piotr.maszota@leancode.co>
2025-03-22 20:42:57 +0100

    Use workaround for iOS release

--- dev/e2e_app/integration_test/enter_text_test.dart
+++ dev/e2e_app/integration_test/enter_text_test.dart
@@ -17,11 +17,14 @@ void main() {
       // Enter username and tap next
       await $(Key('username_text_field')).enterText('test');
       await $(Key('username_text_field')).enterText('test123');
+      expect($('test123'), findsOneWidget);
+
       await $.tap(find.byKey(Key('username_next_button')));
 
       // Enter password and tap next
       await $(Key('password_text_field')).enterText('123456');
       await $(Key('password_text_field')).enterText('123456789');
+      expect($('123456789'), findsOneWidget);
 
       await $(Key('password_back_button')).tap();
 
@@ -30,7 +33,7 @@ void main() {
       // Enter password and tap next
       await $(Key('password_text_field')).enterText('123456');
       await $(Key('password_text_field')).enterText('123456789');
-
+      expect($('123456789'), findsOneWidget);
       await $.tap(find.byKey(Key('password_next_button')));
 
       // Verify we're on the welcome page with correct username
