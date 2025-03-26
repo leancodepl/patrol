79b6a130d9c140dd74ffc5fb356dd97a1c49460b
piotruela <piotr.maszota@leancode.co>
2025-03-26 12:21:03 +0100

    test: Extract keys for autofocus_textfield_test.dart and remove redundant steps

--- dev/e2e_app/integration_test/autofocus_textfield_test.dart
+++ dev/e2e_app/integration_test/autofocus_textfield_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:e2e_app/keys.dart';
 
 import 'common.dart';
 
@@ -15,29 +15,21 @@ void main() {
       await $.pumpAndSettle();
 
       // Enter username and tap next
-      await $(Key('username_text_field')).enterText('test');
-      await $(Key('username_text_field')).enterText('test123');
+      await $(K.usernameTextField).enterText('test');
+      await $(K.usernameTextField).enterText('test123');
       expect($('test123'), findsOneWidget);
 
-      await $.tap(find.byKey(Key('username_next_button')));
+      await $(K.usernameNextButton).tap();
 
       // Enter password and tap next
-      await $(Key('password_text_field')).enterText('123456');
-      await $(Key('password_text_field')).enterText('123456789');
+      await $(K.passwordTextField).enterText('123456');
+      await $(K.passwordTextField).enterText('123456789');
       expect($('123456789'), findsOneWidget);
 
-      await $(Key('password_back_button')).tap();
-
-      await $.tap(find.byKey(Key('username_next_button')));
-
-      // Enter password and tap next
-      await $(Key('password_text_field')).enterText('123456');
-      await $(Key('password_text_field')).enterText('123456789');
-      expect($('123456789'), findsOneWidget);
-      await $.tap(find.byKey(Key('password_next_button')));
+      await $(K.passwordNextButton).tap();
 
       // Verify we're on the welcome page with correct username
-      expect(find.byKey(Key('welcome_text')), findsOneWidget);
+      expect($(K.welcomeText), findsOneWidget);
     },
     tags: ['enterText'],
   );
