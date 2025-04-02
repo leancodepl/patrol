ccd62272fd0a5bf880c23d959e4c0ce5f161b077
piotruela <piotr.maszota@leancode.co>
2025-04-02 13:01:27 +0200

    chore: Enable desugaring for e2e app

--- dev/e2e_app/android/app/build.gradle.kts
+++ dev/e2e_app/android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
