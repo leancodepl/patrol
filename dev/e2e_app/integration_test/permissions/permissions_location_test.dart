b2443b9b2eda274935315934e055691824362cba
piotruela <piotr.maszota@leancode.co>
2025-04-03 10:49:15 +0200

    fix: Add comment explaining increased timeout duration

--- dev/e2e_app/integration_test/permissions/permissions_location_test.dart
+++ dev/e2e_app/integration_test/permissions/permissions_location_test.dart
@@ -70,10 +70,14 @@ void main() {
     }
 
     expect(
+      // timeout duration is increased here as the location service on CI
+      // needs more time to start up
       await $(RegExp('lat')).waitUntilVisible(timeout: Duration(seconds: 20)),
       findsOneWidget,
     );
     expect(
+      // timeout duration is increased here as the location service on CI
+      // needs more time to start up
       await $(RegExp('lng')).waitUntilVisible(timeout: Duration(seconds: 20)),
       findsOneWidget,
     );
@@ -98,10 +102,14 @@ void main() {
     }
 
     expect(
+      // timeout duration is increased here as the location service on CI
+      // needs more time to start up
       await $(RegExp('lat')).waitUntilVisible(timeout: Duration(seconds: 30)),
       findsOneWidget,
     );
     expect(
+      // timeout duration is increased here as the location service on CI
+      // needs more time to start up
       await $(RegExp('lng')).waitUntilVisible(timeout: Duration(seconds: 30)),
       findsOneWidget,
     );
