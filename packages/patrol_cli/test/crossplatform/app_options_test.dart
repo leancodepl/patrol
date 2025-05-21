336167f55768948db085dbcb35197e39cb2945d0
piotruela <piotr.maszota@leancode.co>
2025-05-21 22:31:41 +0200

    Update tests

--- packages/patrol_cli/test/crossplatform/app_options_test.dart
+++ packages/patrol_cli/test/crossplatform/app_options_test.dart
@@ -104,7 +104,8 @@ void main() {
             r'.\gradlew.bat',
             ':app:assembleDevReleaseAndroidTest',
             r'-Ptarget=C:\Users\john\app\integration_test\app_test.dart',
-            '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbQ==,UEFTU1dPUkQ9bnk0bmNhdA==,Zm9vPWJhcg==,RkxVVFRFUl9BUFBfRkxBVk9SPWRldg==',
+            '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbQ==,UEFTU1dPUkQ9bnk0bmNhdA==,Zm9vPWJhcg==',
+            '-Papp-server-port=1',
             '-Ptest-server-port=2',
           ]),
         );
@@ -134,7 +135,8 @@ void main() {
             './gradlew',
             ':app:assembleDevDebugAndroidTest',
             '-Ptarget=/Users/john/app/integration_test/app_test.dart',
-            '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbQ==,UEFTU1dPUkQ9bnk0bmNhdA==,Zm9vPWJhcg==,RkxVVFRFUl9BUFBfRkxBVk9SPWRldg==',
+            '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbQ==,UEFTU1dPUkQ9bnk0bmNhdA==,Zm9vPWJhcg==',
+            '-Papp-server-port=1',
             '-Ptest-server-port=2',
           ]),
         );
@@ -164,7 +166,7 @@ void main() {
             './gradlew',
             ':app:connectedDevDebugAndroidTest',
             '-Ptarget=/Users/john/app/integration_test/app_test.dart',
-            '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbQ==,UEFTU1dPUkQ9bnk0bmNhdA==,Zm9vPWJhcg==,RkxVVFRFUl9BUFBfRkxBVk9SPWRldg==',
+            '-Pdart-defines=RU1BSUw9dXNlckBleGFtcGxlLmNvbQ==,UEFTU1dPUkQ9bnk0bmNhdA==,Zm9vPWJhcg==',
             '-Pandroid.injected.androidTest.leaveApksInstalledAfterRun=true',
             '-Ptest-server-port=2',
           ]),
