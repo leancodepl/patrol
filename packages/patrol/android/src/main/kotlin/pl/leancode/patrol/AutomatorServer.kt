f9c5139cfae33c85d69f0a878e59011a57667f20
Kwiatek Szymon <ken1998@wp.pl>
2025-06-12 16:45:55 +0200

    Fix kotlin formatting issues

--- packages/patrol/android/src/main/kotlin/pl/leancode/patrol/AutomatorServer.kt
+++ packages/patrol/android/src/main/kotlin/pl/leancode/patrol/AutomatorServer.kt
@@ -351,15 +351,15 @@ class AutomatorServer(private val automation: Automator) : NativeAutomatorServer
     }
 
     override fun isSimulator(): IsSimulatorResponse {
-        val isEmulator = Build.FINGERPRINT.startsWith("generic")
-                || Build.FINGERPRINT.startsWith("unknown")
-                || Build.MODEL.contains("google_sdk")
-                || Build.MODEL.contains("Emulator")
-                || Build.MODEL.contains("Android SDK built for x86")
-                || Build.MANUFACTURER.contains("Genymotion")
-                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-                || "google_sdk" == Build.PRODUCT
-        
+        val isEmulator = Build.FINGERPRINT.startsWith("generic") ||
+            Build.FINGERPRINT.startsWith("unknown") ||
+            Build.MODEL.contains("google_sdk") ||
+            Build.MODEL.contains("Emulator") ||
+            Build.MODEL.contains("Android SDK built for x86") ||
+            Build.MANUFACTURER.contains("Genymotion") ||
+            (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")) ||
+            "google_sdk" == Build.PRODUCT
+
         return IsSimulatorResponse(isEmulator)
     }
 }
