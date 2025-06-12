d4ba0c6993425ab13ff2f046da9ca65ccbabc3c6
piotruela <piotr.maszota@leancode.co>
2025-06-12 17:53:09 +0200

    Prepare patrol 3.16.1-dev.1 to release

--- packages/patrol_cli/lib/src/compatibility_checker/version_compatibility.dart
+++ packages/patrol_cli/lib/src/compatibility_checker/version_compatibility.dart
@@ -89,7 +89,7 @@ class VersionCompatibility {
 final List<VersionCompatibility> versionCompatibilityList = [
   VersionCompatibility.fromRangeString(
     patrolCliVersion: '3.6.0-dev.1 - 3.6.1-dev.1',
-    patrolVersion: '3.16.0-dev.1 - 3.16.0-dev.3',
+    patrolVersion: '3.16.0-dev.1 - 3.16.1-dev.1',
     minFlutterVersion: '3.24.0',
   ),
   VersionCompatibility.fromRangeString(
