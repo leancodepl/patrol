e59664450b9c5eba8e2379680fceeb889806a08f
Kwiatek Szymon <ken1998@wp.pl>
2025-06-13 15:02:50 +0200

    Fix custom lint warnings

--- dev/e2e_app/integration_test/camera_features/pick_multiple_photos_from_gallery_test.dart
+++ dev/e2e_app/integration_test/camera_features/pick_multiple_photos_from_gallery_test.dart
@@ -7,7 +7,7 @@ void main() {
     await createApp($);
     if (await $.native2.isSimulator()) {
       await $.native.openApp(appId: 'com.android.camera2');
-      await $.native.tap(Selector(text: "NEXT"));
+      await $.native.tap(Selector(text: 'NEXT'));
       if (await $.native2.isPermissionDialogVisible(
         timeout: const Duration(seconds: 4),
       )) {
@@ -18,7 +18,7 @@ void main() {
       await $.native
           .tap(Selector(resourceId: 'com.android.camera2:id/shutter_button'));
 
-      await Future.delayed(const Duration(seconds: 8));
+      await Future.delayed(const Duration(seconds: 4), () {});
 
       await $.native.openApp(appId: 'pl.leancode.patrol.e2e_app');
     } else {
@@ -28,14 +28,23 @@ void main() {
       )) {
         await $.native2.grantPermissionWhenInUse();
       }
-      await $.native.tap(Selector(text: "Done"));
-      await $.native.tap(Selector(
-          resourceId: 'com.google.android.GoogleCamera:id/shutter_button'));
-      await $.native.tap(Selector(
-          resourceId: 'com.google.android.GoogleCamera:id/shutter_button'));
-      await $.native.tap(Selector(
-          resourceId: 'com.google.android.GoogleCamera:id/shutter_button'));
-      await Future.delayed(const Duration(seconds: 8));
+      await $.native.tap(Selector(text: 'Done'));
+      await $.native.tap(
+        Selector(
+          resourceId: 'com.google.android.GoogleCamera:id/shutter_button',
+        ),
+      );
+      await $.native.tap(
+        Selector(
+          resourceId: 'com.google.android.GoogleCamera:id/shutter_button',
+        ),
+      );
+      await $.native.tap(
+        Selector(
+          resourceId: 'com.google.android.GoogleCamera:id/shutter_button',
+        ),
+      );
+      await Future.delayed(const Duration(microseconds: 4), () {});
       await $.native.openApp(appId: 'pl.leancode.patrol.e2e_app');
     }
     await $(#cameraFeaturesButton).scrollTo().tap();
