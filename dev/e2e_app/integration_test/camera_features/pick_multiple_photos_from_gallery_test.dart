587b015d018d75bf9abf006d9b01b26503b3f664
Kwiatek Szymon <ken1998@wp.pl>
2025-06-13 14:19:10 +0200

    Make some photos with camera to be able to test picking from gallery

--- dev/e2e_app/integration_test/camera_features/pick_multiple_photos_from_gallery_test.dart
+++ dev/e2e_app/integration_test/camera_features/pick_multiple_photos_from_gallery_test.dart
@@ -1,9 +1,43 @@
+import 'package:flutter/material.dart';
+
 import '../common.dart';
 
 void main() {
   patrol('pick multiple images from gallery', ($) async {
     await createApp($);
+    if (await $.native2.isSimulator()) {
+      await $.native.openApp(appId: 'com.android.camera2');
+      await $.native.tap(Selector(text: "NEXT"));
+      if (await $.native2.isPermissionDialogVisible(
+        timeout: const Duration(seconds: 4),
+      )) {
+        await $.native2.grantPermissionWhenInUse();
+      }
+      await $.native
+          .tap(Selector(resourceId: 'com.android.camera2:id/shutter_button'));
+      await $.native
+          .tap(Selector(resourceId: 'com.android.camera2:id/shutter_button'));
 
+      await Future.delayed(const Duration(seconds: 8));
+
+      await $.native.openApp(appId: 'pl.leancode.patrol.e2e_app');
+    } else {
+      await $.native.openApp(appId: 'com.google.android.GoogleCamera');
+      if (await $.native2.isPermissionDialogVisible(
+        timeout: const Duration(seconds: 4),
+      )) {
+        await $.native2.grantPermissionWhenInUse();
+      }
+      await $.native.tap(Selector(text: "Done"));
+      await $.native.tap(Selector(
+          resourceId: 'com.google.android.GoogleCamera:id/shutter_button'));
+      await $.native.tap(Selector(
+          resourceId: 'com.google.android.GoogleCamera:id/shutter_button'));
+      await $.native.tap(Selector(
+          resourceId: 'com.google.android.GoogleCamera:id/shutter_button'));
+      await Future.delayed(const Duration(seconds: 8));
+      await $.native.openApp(appId: 'pl.leancode.patrol.e2e_app');
+    }
     await $(#cameraFeaturesButton).scrollTo().tap();
     await $(#pickMultiplePhotosButton).tap();
     if (await $.native2.isPermissionDialogVisible(
@@ -11,6 +45,9 @@ void main() {
     )) {
       await $.native2.grantPermissionWhenInUse();
     }
+    final nativeViews = await $.native.getNativeViews(null);
+    $.log('nativeViews: $nativeViews');
+    debugPrint('nativeViews: $nativeViews', wrapWidth: 1000);
     await $.native2.pickMultipleImagesFromGallery(2);
 
     await $.pumpAndSettle();
--- dev/e2e_app/integration_test/camera_features/pick_photo_from_gallery_test.dart
+++ dev/e2e_app/integration_test/camera_features/pick_photo_from_gallery_test.dart
@@ -3,7 +3,33 @@ import '../common.dart';
 void main() {
   patrol('pick image from gallery', ($) async {
     await createApp($);
+    if (await $.native2.isSimulator()) {
+      await $.native.openApp(appId: 'com.android.camera2');
+      await $.native.tap(Selector(text: "NEXT"));
+      if (await $.native2.isPermissionDialogVisible(
+        timeout: const Duration(seconds: 4),
+      )) {
+        await $.native2.grantPermissionWhenInUse();
+      }
+      await $.native
+          .tap(Selector(resourceId: 'com.android.camera2:id/shutter_button'));
 
+      await Future.delayed(const Duration(seconds: 8));
+
+      await $.native.openApp(appId: 'pl.leancode.patrol.e2e_app');
+    } else {
+      await $.native.openApp(appId: 'com.google.android.GoogleCamera');
+      if (await $.native2.isPermissionDialogVisible(
+        timeout: const Duration(seconds: 4),
+      )) {
+        await $.native2.grantPermissionWhenInUse();
+      }
+      await $.native.tap(Selector(text: "Done"));
+      await $.native.tap(Selector(
+          resourceId: 'com.google.android.GoogleCamera:id/shutter_button'));
+      await Future.delayed(const Duration(seconds: 4));
+      await $.native.openApp(appId: 'pl.leancode.patrol.e2e_app');
+    }
     await $(#cameraFeaturesButton).scrollTo().tap();
     await $(#chooseFromGalleryButton).tap();
     if (await $.native2.isPermissionDialogVisible(
