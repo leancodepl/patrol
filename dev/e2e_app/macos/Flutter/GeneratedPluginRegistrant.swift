b22e094cc9c27714b7260fb20eb677264f09b36d
Kwiatek Szymon <ken1998@wp.pl>
2025-06-24 17:32:58 +0200

    Add Patrol plugin registration in GeneratedPluginRegistrant.swift

--- dev/e2e_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dev/e2e_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_selector_macos
 import flutter_local_notifications
 import flutter_timezone
 import geolocator_apple
+import patrol
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PatrolPlugin.register(with: registry.registrar(forPlugin: "PatrolPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
