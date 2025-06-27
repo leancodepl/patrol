76895087e00d5ff75cffbd4b9b5f52d96549bd97
Kwiatek Szymon <ken1998@wp.pl>
2025-06-27 13:50:29 +0200

    Revert change in GeneratedPlugin

--- dev/e2e_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dev/e2e_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -20,5 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PatrolPlugin.register(with: registry.registrar(forPlugin: "PatrolPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
