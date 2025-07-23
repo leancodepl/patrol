ca15e1351b108ff174dec0172450813afd465ecd
Kwiatek Szymon <ken1998@wp.pl>
2025-07-23 10:42:21 +0200

    Add TODO comment for future tag handling in Bluetooth integration test

--- dev/e2e_app/integration_test/service_bluetooth_test.dart
+++ dev/e2e_app/integration_test/service_bluetooth_test.dart
@@ -5,6 +5,7 @@ import 'common.dart';
 void main() {
   patrol('disables and enables bluetooth twice', ($) async {
     await createApp($);
+    // TODO: will be handled with tags in the future
     if (await $.native.isVirtualDevice() && Platform.isIOS) {
       $.log(
         'Test will be skipped because of iOS simulator limitations',
