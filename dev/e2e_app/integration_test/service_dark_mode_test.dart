14274e8c9972330e40c177090e0fca9bfb5f30b3
Kwiatek Szymon <ken1998@wp.pl>
2025-07-24 21:43:18 +0200

    Removing not true if, because darkmode should work on simulator

--- dev/e2e_app/integration_test/service_dark_mode_test.dart
+++ dev/e2e_app/integration_test/service_dark_mode_test.dart
@@ -5,13 +5,9 @@ import 'common.dart';
 void main() {
   patrol('disables and enables dark mode twice', ($) async {
     await createApp($);
-    if (await $.native.isVirtualDevice() && Platform.isIOS) {
-      $.log('Test will be skipped because of iOS simulator limitations');
-    } else {
-      await $.native.disableDarkMode();
-      await $.native.enableDarkMode();
-      await $.native.disableDarkMode();
-      await $.native.enableDarkMode();
-    }
+    await $.native.disableDarkMode();
+    await $.native.enableDarkMode();
+    await $.native.disableDarkMode();
+    await $.native.enableDarkMode();
   }, tags: ['locale_testing_ios']);
 }
