d9a8b6ea8adb37b0e57e6419d234a388eac00847
Krzysztof Mamak <krzysztof.mamak@leancode.pl>
2025-09-04 15:41:22 +0200

    Add app version patrol test

---
+++ packages/patrol/example/integration_test/app_version_test.dart
@@ -0,0 +1,18 @@
+import 'package:example/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+
+void main() {
+  // This test must be run with correct build-name (1.2.3) and
+  // build-number (123) flags.
+  patrolTest('displays correct app version', ($) async {
+    const key = Key('homePage_appVersion');
+
+    await $.pumpWidgetAndSettle(const MyApp());
+
+    await $(key).waitUntilVisible();
+
+    expect($(key).text, equals('App version: 1.2.3+123'));
+  });
+}
