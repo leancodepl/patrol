e37137baa5b2a99c6d718ce50e52788712a45242
Krzysztof Mamak <krzysztof.mamak@leancode.pl>
2025-09-12 14:15:03 +0200

    Rename flag (clear-package-data->full-isolation), use full-isolation flag to override clearPackageData value on Android

--- packages/patrol_cli/lib/src/commands/build_ios.dart
+++ packages/patrol_cli/lib/src/commands/build_ios.dart
@@ -44,6 +44,7 @@ class BuildIOSCommand extends PatrolCommand {
     usesCheckCompatibilityOption();
     usesBuildNameOption();
     usesBuildNumberOption();
+    usesFullIsolationOption();
 
     usesIOSOptions();
     argParser.addFlag(
@@ -189,7 +190,7 @@ class BuildIOSCommand extends PatrolCommand {
       osVersion: stringArg('ios') ?? 'latest',
       appServerPort: super.appServerPort,
       testServerPort: super.testServerPort,
-      clearPackageData: boolArg('clear-package-data'),
+      fullIsolation: boolArg('full-isolation'),
     );
 
     try {
