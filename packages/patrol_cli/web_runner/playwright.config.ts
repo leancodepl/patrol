3d8b7f388035e02ff440f3ee91cfad033b132276
piotruela <piotr.maszota@leancode.co>
2025-09-15 19:01:34 +0200

    Place test run results among tested app files

--- packages/patrol_cli/web_runner/playwright.config.ts
+++ packages/patrol_cli/web_runner/playwright.config.ts
@@ -8,4 +8,13 @@ export default defineConfig({
   },
   globalSetup: require.resolve("./tests/setup"),
   globalTeardown: require.resolve("./tests/teardown"),
+  // Output test results to the tested app directory
+  outputDir: process.env.PATROL_TEST_RESULTS_DIR || "./test-results",
+  reporter: [
+    ["html", { 
+      outputFolder: process.env.PATROL_TEST_REPORT_DIR || "./playwright-report",
+      open: "never"
+    }],
+    ["list"]
+  ],
 });
