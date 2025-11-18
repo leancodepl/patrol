46a1b550f07789baf6c946eb1ab26b51c41a8d97
Julia Borkowska <julia.borkowska@leancode.pl>
2025-11-18 16:07:06 +0100

    Upgrade kotlin in example app, update build.gradle

--- packages/patrol/example/android/settings.gradle.kts
+++ packages/patrol/example/android/settings.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
 }
 
 include(":app")
