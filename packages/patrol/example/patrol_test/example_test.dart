c222dd74a554e903cee39aae8b8951d420b48c29
Aleksander Szczepura <al.szczepura@gmail.com>
2025-11-18 16:50:10 +0100

    Revert example_test.dart changes

--- packages/patrol/example/patrol_test/example_test.dart
+++ packages/patrol/example/patrol_test/example_test.dart
@@ -1,76 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:patrol/patrol.dart';
-import 'package:patrol/src/native/patrol_app_service_web.dart';
 
 void main() {
-  patrolTest('grant geolocation permission', ($) async {
-    await $.pumpWidgetAndSettle(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: const Text('App Test 1')),
-          backgroundColor: Colors.blue,
-          body: const LocationWidget(),
+  patrolTest(
+    'counter state is the same after going to home and switching apps',
+    ($) async {
+      // Replace later with your app's main widget
+      await $.pumpWidgetAndSettle(
+        MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(title: const Text('app')),
+            backgroundColor: Colors.blue,
+          ),
         ),
-      ),
-    );
+      );
 
-    expect($('App Test 1'), findsOneWidget);
-
-    await Future<void>.delayed(const Duration(seconds: 1));
-
-    await $(#check_permission).tap();
-
-    await Future<void>.delayed(const Duration(seconds: 2));
-
-    expect($('Current location permission: denied'), findsOneWidget);
-
-    // Grant geolocation permission via Playwright (native)
-    await callPlaywright('grantPermissions', {
-      'permissions': ['geolocation'],
-    });
-
-    await Future<void>.delayed(const Duration(seconds: 2));
-
-    await $(#check_permission).tap();
-
-    await Future<void>.delayed(const Duration(seconds: 2));
-
-    expect($('Current location permission: whileInUse'), findsOneWidget);
-  });
-}
-
-class LocationWidget extends StatefulWidget {
-  const LocationWidget({super.key});
-
-  @override
-  State<LocationWidget> createState() => _LocationWidgetState();
-}
-
-class _LocationWidgetState extends State<LocationWidget> {
-  LocationPermission? currentLocationPermission;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Text(
-            'Current location permission: ${currentLocationPermission?.name}',
-          ),
-          ElevatedButton(
-            key: const Key('check_permission'),
-            onPressed: () async {
-              final permission = await Geolocator.checkPermission();
-              setState(() {
-                currentLocationPermission = permission;
-              });
-            },
-            child: const Text('Check Permission'),
-          ),
-        ],
-      ),
-    );
-  }
+      expect($('app'), findsOneWidget);
+      if (!Platform.isMacOS) {
+        await $.native.pressHome();
+      }
+    },
+  );
 }
