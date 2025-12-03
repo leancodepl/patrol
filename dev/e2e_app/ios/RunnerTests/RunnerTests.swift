a373375b2da1d354fed32b8d56ead27ec3884945
Aleksander Szczepura <al.szczepura@gmail.com>
2025-12-03 13:33:31 +0100

    Update ios runner tests

--- dev/e2e_app/ios/RunnerTests/RunnerTests.swift
+++ dev/e2e_app/ios/RunnerTests/RunnerTests.swift
@@ -1,17 +1,13 @@
-// FIXME: Change to IOSSelector tests
-/* import XCTest
+import XCTest
 import patrol
-
-typealias Selector = patrol.Selector
 
 final class RunnerTests: XCTestCase {
   func testSample() {
-    // This test is here to serve as a sample for more tests in the future
     XCTAssertEqual(2 + 2, 4)
   }
 
-  func testSelectorToNSPredicate_text() {
-    var selector = createEmptySelector()
+  func testIOSSelectorToNSPredicate_text() {
+    var selector = createEmptyIOSSelector()
     selector.text = "Log in"
 
     let predicate = selector.toNSPredicate()
@@ -21,12 +17,14 @@ final class RunnerTests: XCTestCase {
       predicate.predicateFormat,
       """
       label == "Log in" OR \
-      title == "Log in"
+      title == "Log in" OR \
+      value == "Log in" OR \
+      placeholderValue == "Log in"
       """)
   }
 
-  func testSelectorToNSPredicate_textStartsWith() {
-    var selector = createEmptySelector()
+  func testIOSSelectorToNSPredicate_textStartsWith() {
+    var selector = createEmptyIOSSelector()
     selector.textStartsWith = "Log in"
 
     let predicate = selector.toNSPredicate()
@@ -36,12 +34,14 @@ final class RunnerTests: XCTestCase {
       predicate.predicateFormat,
       """
       label BEGINSWITH "Log in" OR \
-      title BEGINSWITH "Log in"
+      title BEGINSWITH "Log in" OR \
+      value BEGINSWITH "Log in" OR \
+      placeholderValue BEGINSWITH "Log in"
       """)
   }
 
-  func testSelectorToNSPredicate_textContains() {
-    var selector = createEmptySelector()
+  func testIOSSelectorToNSPredicate_textContains() {
+    var selector = createEmptyIOSSelector()
     selector.textContains = "Log in"
 
     let predicate = selector.toNSPredicate()
@@ -51,13 +51,15 @@ final class RunnerTests: XCTestCase {
       predicate.predicateFormat,
       """
       label CONTAINS "Log in" OR \
-      title CONTAINS "Log in"
+      title CONTAINS "Log in" OR \
+      value CONTAINS "Log in" OR \
+      placeholderValue CONTAINS "Log in"
       """)
   }
 
-  func testSelectorToNSPredicate_resourceId() {
-    var selector = createEmptySelector()
-    selector.resourceId = "log_in"
+  func testIOSSelectorToNSPredicate_identifier() {
+    var selector = createEmptyIOSSelector()
+    selector.identifier = "log_in"
 
     let predicate = selector.toNSPredicate()
 
@@ -69,10 +71,122 @@ final class RunnerTests: XCTestCase {
       """)
   }
 
-  func testSelectorToNSPredicate_complex_1() {
-    var selector = createEmptySelector()
+  func testIOSSelectorToNSPredicate_label() {
+    var selector = createEmptyIOSSelector()
+    selector.label = "Log in"
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      label == "Log in"
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_labelStartsWith() {
+    var selector = createEmptyIOSSelector()
+    selector.labelStartsWith = "Log"
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      label BEGINSWITH "Log"
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_title() {
+    var selector = createEmptyIOSSelector()
+    selector.title = "Log in"
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      title == "Log in"
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_titleContains() {
+    var selector = createEmptyIOSSelector()
+    selector.titleContains = "Log"
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      title CONTAINS "Log"
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_placeholderValue() {
+    var selector = createEmptyIOSSelector()
+    selector.placeholderValue = "Enter username"
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      placeholderValue == "Enter username"
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_value() {
+    var selector = createEmptyIOSSelector()
+    selector.value = "some_value"
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      value == "some_value"
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_isEnabled() {
+    var selector = createEmptyIOSSelector()
+    selector.isEnabled = true
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      isEnabled == YES
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_hasFocus() {
+    var selector = createEmptyIOSSelector()
+    selector.hasFocus = false
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      hasFocus == NO
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_complex_textContainsAndIdentifier() {
+    var selector = createEmptyIOSSelector()
     selector.textContains = "text_contains"
-    selector.resourceId = "resource_id"
+    selector.identifier = "resource_id"
 
     let predicate = selector.toNSPredicate()
 
@@ -81,15 +195,17 @@ final class RunnerTests: XCTestCase {
       predicate.predicateFormat,
       """
       (label CONTAINS "text_contains" OR \
-      title CONTAINS "text_contains") AND \
+      title CONTAINS "text_contains" OR \
+      value CONTAINS "text_contains" OR \
+      placeholderValue CONTAINS "text_contains") AND \
       identifier == "resource_id"
       """)
   }
 
-  func testSelectorToNSPredicate_complex_2() {
-    var selector = createEmptySelector()
-    selector.textContains = "text_contains"
-    selector.resourceId = "resource_id"
+  func testIOSSelectorToNSPredicate_complex_labelAndTitle() {
+    var selector = createEmptyIOSSelector()
+    selector.label = "Welcome"
+    selector.titleContains = "back"
 
     let predicate = selector.toNSPredicate()
 
@@ -97,20 +213,38 @@ final class RunnerTests: XCTestCase {
     XCTAssertEqual(
       predicate.predicateFormat,
       """
-      (label CONTAINS "text_contains" OR \
-      title CONTAINS "text_contains") AND \
-      identifier == "resource_id"
+      label == "Welcome" AND \
+      title CONTAINS "back"
+      """)
+  }
+
+  func testIOSSelectorToNSPredicate_complex_multipleProperties() {
+    var selector = createEmptyIOSSelector()
+    selector.textContains = "search"
+    selector.isEnabled = true
+    selector.identifier = "search_field"
+
+    let predicate = selector.toNSPredicate()
+
+    NSLog(predicate.predicateFormat)
+    XCTAssertEqual(
+      predicate.predicateFormat,
+      """
+      (label CONTAINS "search" OR \
+      title CONTAINS "search" OR \
+      value CONTAINS "search" OR \
+      placeholderValue CONTAINS "search") AND \
+      isEnabled == YES AND \
+      identifier == "search_field"
       """)
   }
 }
 
-private func createEmptySelector(text: String? = nil) -> patrol.Selector {
-  // Temporary fix. We will remove the Selector class later
+private func createEmptyIOSSelector() -> patrol.IOSSelector {
   let jsonString = "{}"
 
   let jsonData = jsonString.data(using: .utf8)!
   let decoder = JSONDecoder()
 
-  return try! decoder.decode(patrol.Selector.self, from: jsonData)
+  return try! decoder.decode(patrol.IOSSelector.self, from: jsonData)
 }
- */
