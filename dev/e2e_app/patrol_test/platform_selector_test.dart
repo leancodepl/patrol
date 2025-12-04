b9c7836eb1e9ccbc4f9c74d2f214fc5700018b40
Filip Tuzimek <ftuzimek@gmail.com>
2025-12-04 17:37:59 +0100

    Add E2E test

---
+++ dev/e2e_app/patrol_test/platform_selector_test.dart
@@ -0,0 +1,19 @@
+import 'common.dart';
+
+void main() {
+  patrol('PlatformSelector allows different selectors per platform', ($) async {
+    await createApp($);
+
+    await $('Open webview (Hacker News)').scrollTo().tap();
+    await $.pump(Duration(seconds: 5));
+
+    // Use PlatformSelector when iOS and Android need different selectors
+    await $.platform.mobile.tap(
+      PlatformSelector(
+        android: AndroidSelector(text: 'login'),
+        ios: IOSSelector(label: 'login'),
+        web: WebSelector(text: 'login'),
+      ),
+    );
+  });
+}
