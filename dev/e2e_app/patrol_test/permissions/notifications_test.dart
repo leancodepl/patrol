081085f96862b26b983aad7d59f938c1286e3164
Oskar Zajac <oskar.zajac@leancode.pl>
2026-02-09 06:08:57 +0100

    remove getNotifications test

--- dev/e2e_app/patrol_test/permissions/notifications_test.dart
+++ dev/e2e_app/patrol_test/permissions/notifications_test.dart
@@ -64,30 +64,6 @@ void main() {
     await $.platform.mobile.tapOnNotificationByIndex(0);
     await $('Tapped notification with ID: 1').waitUntilVisible();
   });
-
-  // Regression test: on iOS 18+, the notification list includes a header element
-  // that should be filtered out by getNotifications() to match tapOnNotificationByIndex()
-  patrol('getNotifications returns only real notifications', ($) async {
-    await createApp($);
-    await $('Open notifications screen').scrollTo().tap();
-
-    if (await $.platform.mobile.isPermissionDialogVisible()) {
-      await $.platform.mobile.grantPermissionWhenInUse();
-    }
-    await handleAndroid14NotificationPermission($);
-    await $(K.showNotificationNowButton).tap();
-    if (io.Platform.isIOS) {
-      await $.platform.ios.closeHeadsUpNotification();
-    }
-    await $.platform.mobile.openNotifications();
-
-    final notifications = await $.platform.mobile.getNotifications();
-    expect(notifications.length, equals(1));
-    expect(notifications[0].raw, contains('Someone liked'));
-
-    await $.platform.mobile.tapOnNotificationByIndex(0);
-    await $('Tapped notification with ID: 1').waitUntilVisible();
-  });
 }
 
 /// Android 14+ requires additional permission to schedule notifications.
