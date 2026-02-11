98994d905f2d20b67932a5681e5baf3bd48564ff
Julia Borkowska <julia.borkowska@leancode.pl>
2026-02-11 15:04:24 +0100

    fix arg name

--- packages/patrol/darwin/Classes/AutomatorServer/Automator/IOSAutomator.swift
+++ packages/patrol/darwin/Classes/AutomatorServer/Automator/IOSAutomator.swift
@@ -981,7 +981,7 @@
     }
 
     func axeA11yScan(bundleId: String) throws {
-      let app = getApp(bundleId: bundleId)
+      let app = getApp(withBundleId: bundleId)
       let result = try axe?.run(onElement: app)
       let path = try axe?.saveResult(result)
       Logger.shared.i("Result saved to location: \(path)")
