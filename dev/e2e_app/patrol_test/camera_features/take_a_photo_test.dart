9030546ea65cc44408e57d7ab3ed7ce46f6fff0b
Kwiatek Szymon <ken1998@wp.pl>
2026-03-10 08:39:40 +0100

    Refactor take_a_photo_test.dart to remove iOS simulator camera check and clean up imports

--- dev/e2e_app/patrol_test/camera_features/take_a_photo_test.dart
+++ dev/e2e_app/patrol_test/camera_features/take_a_photo_test.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import '../common.dart';
 import 'camera_helpers.dart';
 
@@ -7,10 +5,8 @@ void main() {
   patrol('take a photo', ($) async {
     await createApp($);
     final cameraHelpers = CameraHelpers($);
+
     await $(#cameraFeaturesButton).scrollTo().tap();
-    if (await $.platform.mobile.isVirtualDevice() && Platform.isIOS) {
-      throw Exception('Camera is not supported on iOS simulator');
-    }
     await $(#takePhotoButton).tap();
     await cameraHelpers.maybeAcceptPermissionDialog();
     await cameraHelpers.maybeAcceptDialogAndroid();
