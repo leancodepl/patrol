6c00d2bbb9d44774ea0853988bbdf1d8a6cdc74b
Matej Resetar <matej.resetar@leancode.pl>
2026-03-12 16:46:30 +0100

    Fix fallback support for CocoaPods

--- packages/patrol/example/ios/RunnerUITests/RunnerUITests.m
+++ packages/patrol/example/ios/RunnerUITests/RunnerUITests.m
@@ -1,6 +1,11 @@
 @import XCTest;
 @import patrol;
 @import ObjectiveC.runtime;
+
+// With CocoaPods, @import patrol exports ObjC headers including this macro.
+// With SPM, @import patrol only exports Swift APIs, so we need explicit #import.
+#if !defined(PATROL_INTEGRATION_TEST_IOS_RUNNER)
 #import "PatrolIntegrationTestIosRunner.h"
+#endif
 
 PATROL_INTEGRATION_TEST_IOS_RUNNER(RunnerUITests)
