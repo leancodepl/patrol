567eee8a592089fee7696d91f36b721716a5ecc0
Kwiatek Szymon <ken1998@wp.pl>
2026-03-16 10:25:27 +0100

    Increase after build completed timeout in patrol_develop_test.dart from 4 to 6 minutes

--- dev/cli_tests/patrol_develop_test.dart
+++ dev/cli_tests/patrol_develop_test.dart
@@ -38,7 +38,7 @@ void main() {
 void main(List<String> args) async {
   _verifyWorkingDirectory();
 
-  const afterBuildCompletedTimeout = Duration(minutes: 4);
+  const afterBuildCompletedTimeout = Duration(minutes: 6);
   const inactivityTimeout = Duration(minutes: 15);
 
   var isFirstTestPassed = false;
