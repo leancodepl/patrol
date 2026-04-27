a8a547e2dab884bc1f9526648b2c6cf1dbd8d067
Natan Dworok <natandworok@DESKTOP-B57FCPA.localdomain>
2026-04-27 16:41:12 +0200

    created test for testing mobile keyboard enter

---
+++ dev/e2e_app/patrol_test/mobile_keyboard_enter_test.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:e2e_app/keys.dart';
+import 'common.dart';
+
+final _iosTextInputActions = TextInputAction.values
+    .where(
+      (action) =>
+          action != TextInputAction.none && action != TextInputAction.previous,
+    )
+    .toList();
+final _androidTextInputActions = TextInputAction.values
+    .where(
+      (action) =>
+          action != TextInputAction.continueAction &&
+          action != TextInputAction.join &&
+          action != TextInputAction.route &&
+          action != TextInputAction.emergencyCall,
+    )
+    .toList();
+
+void main() {
+  patrol(
+    'mobile keyboard enter on text fields',
+    ($) async {
+      final textInputActions = Platform.isAndroid
+          ? _androidTextInputActions
+          : _iosTextInputActions;
+
+      await createApp($);
+
+      await $(K.textfieldsScreenButton).scrollTo().tap();
+
+      for (var index = 0; index < textInputActions.length; index++) {
+        final textField = $(K.textFields[index]);
+        await textField.scrollTo().enterText('test_${textInputActions[index].name}');
+        await textField.tap();
+        await Future<void>.delayed(const Duration(seconds: 1));
+        await $.platform.mobile.sendKeyboardEnter();
+      }
+    
+    },
+    tags: ['android', 'emulator', 'ios', 'simulator'],
+  );
+}
