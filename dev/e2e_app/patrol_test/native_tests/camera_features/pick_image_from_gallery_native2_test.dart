9d9a5b591a0bc9fefb58b3e52febdff17aa2a6f1
Kwiatek Szymon <szymon.kwiatek@leancode.pl>
2026-06-30 09:31:12 +0200

    Revert test; and apply fix

--- dev/e2e_app/patrol_test/native_tests/camera_features/pick_image_from_gallery_native2_test.dart
+++ dev/e2e_app/patrol_test/native_tests/camera_features/pick_image_from_gallery_native2_test.dart
@@ -7,24 +7,30 @@ import '../common.dart';
 import 'camera_helpers.dart';
 
 void main() {
-  patrol('pick image from gallery - native2', ($) async {
-    await createApp($);
-    final cameraHelpers = CameraHelpers($);
-    final isVirtualDevice = await $.native2.isVirtualDevice();
-    if (isVirtualDevice && Platform.isAndroid) {
-      await cameraHelpers.takePhotosAcceptDialogsAndOpenAppOnEmulator();
-    } else if (Platform.isAndroid) {
-      await cameraHelpers
-          .takePhotosAcceptDialogsAndOpenAppOnRealDeviceAndroid();
-    } else if (Platform.isIOS && !isVirtualDevice) {
-      await cameraHelpers.takePhotosAcceptDialogsAndOpenAppOnRealDeviceIOS();
-    }
-    await $(#cameraFeaturesButton).scrollTo().tap();
-    await $(#chooseFromGalleryButton).tap();
-    await cameraHelpers.maybeAcceptPermissionDialog();
-    await $.native2.pickImageFromGallery(index: 1);
-    await $.pumpAndSettle();
+  patrol(
+    'pick image from gallery - native2',
+    ($) async {
+      await createApp($);
+      final cameraHelpers = CameraHelpers($);
+      final isVirtualDevice = await $.native2.isVirtualDevice();
+      if (isVirtualDevice && Platform.isAndroid) {
+        await cameraHelpers.takePhotosAcceptDialogsAndOpenAppOnEmulator();
+      } else if (Platform.isAndroid) {
+        await cameraHelpers
+            .takePhotosAcceptDialogsAndOpenAppOnRealDeviceAndroid();
+      } else if (Platform.isIOS && !isVirtualDevice) {
+        await cameraHelpers.takePhotosAcceptDialogsAndOpenAppOnRealDeviceIOS();
+      }
+      // Wait for photos to be saved in gallery.
+      await Future<void>.delayed(const Duration(seconds: 5));
+      await $(#cameraFeaturesButton).scrollTo().tap();
+      await $(#chooseFromGalleryButton).tap();
+      await cameraHelpers.maybeAcceptPermissionDialog();
+      await $.native2.pickImageFromGallery(index: 1);
+      await $.pumpAndSettle();
 
-    await $(#smallImagePreview).waitUntilVisible();
-  });
+      await $(#smallImagePreview).waitUntilVisible();
+    },
+    tags: ['android', 'emulator', 'physical_device', 'ios'],
+  );
 }
