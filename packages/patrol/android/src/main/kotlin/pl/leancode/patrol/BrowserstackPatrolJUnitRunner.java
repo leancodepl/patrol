061fa3cfd312fb96fc4283ed0b1072f9da6fa099
Aleksander Bielicki <aleksanderbielicki2003@gmail.com>
2026-07-02 13:20:42 +0200

    Add null check

--- packages/patrol/android/src/main/kotlin/pl/leancode/patrol/BrowserstackPatrolJUnitRunner.java
+++ packages/patrol/android/src/main/kotlin/pl/leancode/patrol/BrowserstackPatrolJUnitRunner.java
@@ -25,7 +25,9 @@ public class BrowserstackPatrolJUnitRunner extends PatrolJUnitRunner {
             // If the client on localhost:8082 fails, let's apply the workaround
             Logger.INSTANCE.i("PatrolAppServiceClientException in createAppServiceClient " + ex.getMessage());
             Logger.INSTANCE.i("LOOPBACK: " + getLoopback());
-            client.close();
+            if (client != null) {
+                client.close();
+            }
             client = new PatrolAppServiceClient(getLoopback());
         }
 
